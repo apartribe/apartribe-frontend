85ac0a1f1ea5dae00d83abc0b41943c59a18ed95
HHB <bcl0206@gmail.com>
2023-09-15 06:12:06 +0900

    rename: 일관성을 위한 페이지 이름 변경

--- src/pages/board-pages/AddBoardPostPage.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const AddBoardPostPage = () => {
-  return <div>AddBoardPostPage</div>;
-};
-
-export default AddBoardPostPage;
--- src/pages/board-pages/EditBoardPostPage.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const EditBoardPostPage = () => {
-  return <div>EditBoardPostPage</div>;
-};
-
-export default EditBoardPostPage;
