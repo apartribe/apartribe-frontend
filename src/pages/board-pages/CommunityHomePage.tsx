388ac8e4539e1d3377fa8c0c13e4eee4dd156dc2
ChoongLim <bcl0206@naver.com>
2023-09-14 22:38:38 +0900

    feat: 라우터 구현

--- src/pages/board-pages/CommunityHomePage.tsx
+++ src/pages/board-pages/CommunityHomePage.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import SideSection from "components/common/SideSection";
+import {Outlet} from 'react-router-dom'
 
+// 글쓰기 버튼을 여기에 추가하면 되겠음.
 const CommunityHomePage = () => {
-  return <div>CommunityHomePage</div>;
+  return <>
+    <Outlet/>
+    <SideSection/>
+  </>;
 };
 
 export default CommunityHomePage;
