b990eeb4e74e0d7b858bb2a748946b3e728ed7c5
HHB <bcl0206@gmail.com>
2023-09-16 00:15:13 +0900

    feat: 재사용 인풋 스타일 구현

--- src/styles/reusableStyle.ts
+++ src/styles/reusableStyle.ts
@@ -25,3 +25,23 @@ export const Button = styled.button<reusableStyleProps>`
     cursor: not-allowed;
   }
 `
+
+export const Input = styled.input<reusableStyleProps>`
+  border: none;
+  outline: none;
+  box-sizing: border-box;
+  background: ${(props) => props.$background || '#f2f2f2'};
+  width: ${(props) => props.$width || '100%'};
+  height: ${(props) => props.$height || '55px'};
+  margin: ${(props) => props.$margin || '0px'};
+  padding: ${(props) => props.$padding || '0px  15px'};
+  border-radius: ${(props) => props.$borderRadius || '10px'};
+  color: ${(props) => props.$color || '#303030'};
+  font-size: ${(props) => props.$fontSize || '15px'};
+  font-weight: ${(props) => props.$fontWeight || '400'};
+  letter-spacing: ${(props) => props.$letterSpacing || '0px'};
+
+  ::placeholder {
+    color: #b3b3b3;
+  }
+`
