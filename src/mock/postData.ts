5a5db00ff708cd8b7817d4e0242b8451fa051a07
ChoongLim <bcl0206@naver.com>
2023-09-18 00:20:46 +0900

    feat: 게시물 목록 목데이터 생성

---
+++ src/mock/postData.ts
@@ -0,0 +1,133 @@
+export const POST_MOCK = [
+  {
+    avatar: 'https://upload.wikimedia.org/wikipedia/commons/7/72/Avatar_icon_green.svg',
+    title:
+      '어제 밤에 단지 내에 비명소리 때문에 놀라신 분 있나요?어제 밤에 단지 내에 비명소리 때문에 놀라신 분 있나요?',
+    content:
+      '어제 밤 한시쯤에 아파트 정문 부근에서 여자 비명소리 들으신 분 있나요? 너무 저의 동이 그쪽이라서 정말 큰 소리가 났거든요. 정말 무슨 일이라도 난 것 같아서 창밖을 내다 봤는데 사람은 안 보이더라구요 혹시 무슨 일인지 아시..',
+    date: '1시간전',
+    categoroy: '신혼부부 정보 공유',
+    writer: '호기심 많은 탐험가',
+    view: 63,
+    like: 1,
+    comment: 3,
+    url: 'https://htmlcolorcodes.com/assets/images/colors/dark-red-color-solid-background-1920x1080.png',
+  },
+  {
+    avatar: 'https://upload.wikimedia.org/wikipedia/commons/7/72/Avatar_icon_green.svg',
+    title: '어제 밤에 단지 내에 비명소리 때문에 놀라신 분 있나요?',
+    content:
+      '어제 밤 한시쯤에 아파트 정문 부근에서 여자 비명소리 들으신 분 있나요? 너무 저의 동이 그쪽이라서 정말 큰 소리가 났거든요. 정말 무슨 일이라도 난 것 같아서 창밖을 내다 봤는데 사람은 안 보이더라구요 혹시 무슨 일인지 아시..',
+    date: '1시간전',
+    categoroy: '고양이 집사 모임',
+    writer: '호기심 많은 탐험가',
+    view: 63,
+    like: 2,
+    comment: 3,
+    url: 'https://htmlcolorcodes.com/assets/images/colors/dark-red-color-solid-background-1920x1080.png',
+  },
+  {
+    avatar: 'https://upload.wikimedia.org/wikipedia/commons/7/72/Avatar_icon_green.svg',
+    title: '어제 밤에 단지 내에 비명소리 때문에 놀라신 분 있나요?',
+    content:
+      '어제 밤 한시쯤에 아파트 정문 부근에서 여자 비명소리 들으신 분 있나요? 너무 저의 동이 그쪽이라서 정말 큰 소리가 났거든요. 정말 무슨 일이라도 난 것 같아서 창밖을 내다 봤는데 사람은 안 보이더라구요 혹시 무슨 일인지 아시..',
+    date: '1시간전',
+    categoroy: '비밀 게시판',
+    writer: '호기심 많은 탐험가',
+    view: 63,
+    like: 3,
+    comment: 3,
+    url: 'https://htmlcolorcodes.com/assets/images/colors/dark-red-color-solid-background-1920x1080.png',
+  },
+  {
+    avatar: 'https://upload.wikimedia.org/wikipedia/commons/7/72/Avatar_icon_green.svg',
+    title: '어제 밤에 단지 내에 비명소리 때문에 놀라신 분 있나요?',
+    content:
+      '어제 밤 한시쯤에 아파트 정문 부근에서 여자 비명소리 들으신 분 있나요? 너무 저의 동이 그쪽이라서 정말 큰 소리가 났거든요. 정말 무슨 일이라도 난 것 같아서 창밖을 내다 봤는데 사람은 안 보이더라구요 혹시 무슨 일인지 아시..',
+    date: '1시간전',
+    categoroy: '신혼부부 정보 공유',
+    writer: '호기심 많은 탐험가',
+    view: 63,
+    like: 4,
+    comment: 3,
+    url: '',
+  },
+  {
+    avatar: 'https://upload.wikimedia.org/wikipedia/commons/7/72/Avatar_icon_green.svg',
+    title: '어제 밤에 단지 내에 비명소리 때문에 놀라신 분 있나요?',
+    content:
+      '어제 밤 한시쯤에 아파트 정문 부근에서 여자 비명소리 들으신 분 있나요? 너무 저의 동이 그쪽이라서 정말 큰 소리가 났거든요. 정말 무슨 일이라도 난 것 같아서 창밖을 내다 봤는데 사람은 안 보이더라구요 혹시 무슨 일인지 아시..',
+    date: '1시간전',
+    categoroy: '고양이 집사 모임',
+    writer: '호기심 많은 탐험가',
+    view: 63,
+    like: 5,
+    comment: 3,
+    url: '',
+  },
+  {
+    avatar: 'https://upload.wikimedia.org/wikipedia/commons/7/72/Avatar_icon_green.svg',
+    title: '어제 밤에 단지 내에 비명소리 때문에 놀라신 분 있나요?',
+    content:
+      '어제 밤 한시쯤에 아파트 정문 부근에서 여자 비명소리 들으신 분 있나요? 너무 저의 동이 그쪽이라서 정말 큰 소리가 났거든요. 정말 무슨 일이라도 난 것 같아서 창밖을 내다 봤는데 사람은 안 보이더라구요 혹시 무슨 일인지 아시..',
+    date: '1시간전',
+    categoroy: '비밀 게시판',
+    writer: '호기심 많은 탐험가',
+    view: 63,
+    like: 6,
+    comment: 3,
+    url: '',
+  },
+  {
+    avatar: 'https://upload.wikimedia.org/wikipedia/commons/7/72/Avatar_icon_green.svg',
+    title: '어제 밤에 단지 내에 비명소리 때문에 놀라신 분 있나요?',
+    content:
+      '어제 밤 한시쯤에 아파트 정문 부근에서 여자 비명소리 들으신 분 있나요? 너무 저의 동이 그쪽이라서 정말 큰 소리가 났거든요. 정말 무슨 일이라도 난 것 같아서 창밖을 내다 봤는데 사람은 안 보이더라구요 혹시 무슨 일인지 아시..',
+    date: '1시간전',
+    categoroy: '신혼부부 정보 공유',
+    writer: '호기심 많은 탐험가',
+    view: 63,
+    like: 7,
+    comment: 3,
+    url: 'https://htmlcolorcodes.com/assets/images/colors/dark-red-color-solid-background-1920x1080.png',
+  },
+  {
+    avatar: 'https://upload.wikimedia.org/wikipedia/commons/7/72/Avatar_icon_green.svg',
+    title: '어제 밤에 단지 내에 비명소리 때문에 놀라신 분 있나요?',
+    content:
+      '어제 밤 한시쯤에 아파트 정문 부근에서 여자 비명소리 들으신 분 있나요? 너무 저의 동이 그쪽이라서 정말 큰 소리가 났거든요. 정말 무슨 일이라도 난 것 같아서 창밖을 내다 봤는데 사람은 안 보이더라구요 혹시 무슨 일인지 아시..',
+    date: '1시간전',
+    categoroy: '고양이 집사 모임',
+    writer: '호기심 많은 탐험가',
+    view: 63,
+    like: 8,
+    comment: 3,
+    url: 'https://htmlcolorcodes.com/assets/images/colors/dark-red-color-solid-background-1920x1080.png',
+  },
+  {
+    avatar: 'https://upload.wikimedia.org/wikipedia/commons/7/72/Avatar_icon_green.svg',
+    title: '어제 밤에 단지 내에 비명소리 때문에 놀라신 분 있나요?',
+    content:
+      '어제 밤 한시쯤에 아파트 정문 부근에서 여자 비명소리 들으신 분 있나요? 너무 저의 동이 그쪽이라서 정말 큰 소리가 났거든요. 정말 무슨 일이라도 난 것 같아서 창밖을 내다 봤는데 사람은 안 보이더라구요 혹시 무슨 일인지 아시..',
+    date: '1시간전',
+    categoroy: '비밀 게시판',
+    writer: '호기심 많은 탐험가',
+    view: 63,
+    like: 9,
+    comment: 3,
+    url: 'https://htmlcolorcodes.com/assets/images/colors/dark-red-color-solid-background-1920x1080.png',
+  },
+]
+
+export interface MockType {
+  avatar: string
+  title: string
+  content: string
+  date: string
+  categoroy: string
+  writer: string
+  view: number
+  like: number
+  comment: number
+  url: string
+}
