578fa6b6402c532affa499d4972e2f961ba44d8f
jieeeun2 <jieun2.dev@gmail.com>
2023-09-18 18:22:09 +0900

    feat: 회원가입, 로그인 공통 컴포넌트

---
+++ src/components/auth/AuthLayout.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from 'react'
+import { ShadowBox } from 'styles/reusable-style/elementStyle'
+import { Container, Inner } from 'styles/reusable-style/layoutStyle'
+
+type AuthLayoutProps = {
+  children: ReactNode
+}
+
+const AuthLayout = ({ children }: AuthLayoutProps) => {
+  return (
+    <Container>
+      <Inner $width="550px" $padding="100px 0">
+        <ShadowBox
+          $display="flex"
+          $flexDirection="column"
+          $alignItems="center"
+          $width="500px"
+          $padding="30px 20px"
+        >
+          {children}
+        </ShadowBox>
+      </Inner>
+    </Container>
+  )
+}
+
+export default AuthLayout
