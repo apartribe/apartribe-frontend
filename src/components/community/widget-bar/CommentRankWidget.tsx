44df118d813a9dbd825c526cb25b1ba6ff337fa7
HHB <bcl0206@gmail.com>
2023-09-19 20:41:30 +0900

    feat: 댓글 랭킹 위젯 구현

--- src/components/community/widget-bar/CommentRankWidget.tsx
+++ src/components/community/widget-bar/CommentRankWidget.tsx
@@ -1,9 +1,19 @@
 import WidgetTitleArea from 'components/common/WidgetTitleArea'
 import React from 'react'
-import { ShadowBox } from 'styles/reusable-style/elementStyle'
+import { Img, P, ShadowBox } from 'styles/reusable-style/elementStyle'
 import { FaRankingStar } from 'react-icons/fa6'
+import { COMMENT_RANK_MOCK } from 'mock/commentRankData'
+import { styled } from 'styled-components'
+import { FaTrophy } from 'react-icons/fa'
 
 const CommentRankWidget = () => {
+  const decideIcon = (index: number) => {
+    if (index === 0) return <FaTrophy color="#D5A11E" />
+    if (index === 1) return <FaTrophy color="#A3A3A3" />
+    if (index === 2) return <FaTrophy color="#CD7F32" />
+    return index + 1
+  }
+
   return (
     <ShadowBox>
       <WidgetTitleArea
@@ -11,9 +21,37 @@ const CommentRankWidget = () => {
         title="지난 주 댓글 랭킹"
         hasSeeMore={false}
       />
-      CommentRankWidget
+      {COMMENT_RANK_MOCK.data.map((item, index) => (
+        <StyledWrapper key={item.nickname}>
+          <StyledDiv className="rank">{decideIcon(index)}</StyledDiv>
+          <Img src={item.avatar} $width="40px" $height="40px" />
+          <StyledDiv>
+            <P $fontWeight="700" $lineHeight="25px">
+              {item.nickname}
+            </P>
+            <P $fontSize="11px" $lineHeight="15px">
+              댓글 {item.commentCount}개
+            </P>
+          </StyledDiv>
+        </StyledWrapper>
+      ))}
     </ShadowBox>
   )
 }
 
 export default CommentRankWidget
+
+const StyledWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 15px;
+  padding: 5px 10px;
+`
+
+const StyledDiv = styled.div`
+  &.rank {
+    width: 20px;
+    font-weight: 700;
+    color: #303030;
+  }
+`
