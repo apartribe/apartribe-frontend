14aea625266397724182a85ca1ef567a40da8406
HHB <bcl0206@gmail.com>
2023-09-19 20:40:21 +0900

    feat: 베스트 게시물 위젯 구현

--- src/components/community/widget-bar/BestPostsWidget.tsx
+++ src/components/community/widget-bar/BestPostsWidget.tsx
@@ -2,14 +2,31 @@ import WidgetTitleArea from 'components/common/WidgetTitleArea'
 import React from 'react'
 import { ShadowBox } from 'styles/reusable-style/elementStyle'
 import { FaRankingStar } from 'react-icons/fa6'
+import { styled } from 'styled-components'
+import { BEST_POST_MOCK } from 'mock/bestPostData'
 
 const BestPostsWidget = () => {
   return (
     <ShadowBox>
-      <WidgetTitleArea Icon={FaRankingStar} title="베스트 글" hasSeeMore={false} />
-      BestPostsWidget
+      <WidgetTitleArea Icon={FaRankingStar} title="베스트 게시물" hasSeeMore={false} />
+      {BEST_POST_MOCK.data.map((item) => (
+        <StyledParagraph key={item.id}>{item.title}</StyledParagraph>
+      ))}
     </ShadowBox>
   )
 }
 
 export default BestPostsWidget
+
+const StyledParagraph = styled.div`
+  margin: 0 5px;
+  border-top: 1px solid #f2f2f2;
+  line-height: 35px;
+  font-size: 13px;
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
+  text-overflow: ellipsis;
+  cursor: pointer;
+`
