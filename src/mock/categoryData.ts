64da246d809be62d6d456fb504533daa7dbb2ee9
HHB <bcl0206@gmail.com>
2023-09-19 09:50:22 +0900

    feat: 공지사항, 구인공고 카테고리 생성 및 상수 분리

--- src/mock/categoryData.ts
+++ src/mock/categoryData.ts
@@ -1,4 +1,4 @@
-export const CATEGOTY_LIST_MOCK = [
+export const BOARD_CATEGOTY_LIST_MOCK = [
   '전체',
   '신혼부부 정보 공유',
   '고양이 집사 모임',
@@ -7,4 +7,15 @@ export const CATEGOTY_LIST_MOCK = [
   '신혼부부 정보 공유',
   '고양이 집사 모임',
   '비밀 게시판',
+]
+
+export const ANNOUNCE_CATEGOTY_LIST_MOCK = ['일반', '긴급', '비상']
+
+export const GATHER_PEOPLE_CATEGOTY_LIST_MOCK = [
+  '전체',
+  '동호회',
+  '독서 모임',
+  '친목회',
+  '카풀',
+  '공동구매',
 ]
