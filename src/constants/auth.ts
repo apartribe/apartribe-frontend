36ca8f27cda04f24e379c89be9df4f95e1a9b5bb
jieeeun2 <jieun2.dev@gmail.com>
2023-09-19 16:10:03 +0900

    feat: auth관련 페이지 경로 상수화

---
+++ src/constants/auth.ts
@@ -0,0 +1,11 @@
+export const PAGE_SIGNUP_SELECT = '/signup/select'
+export const PAGE_LOCAL_SIGNUP = '/signup/local'
+export const PAGE_LOGIN = '/login'
+export const PAGE_FIND_ID = '/find/id'
+export const PAGE_FIND_PW = '/find/pw'
+export const PAGE_RESET_PW = '/find/pw/reset'
+
+//export const KAKAO_AUTH_URL = `${BASE_URL}/oauth2/authorization/kakao`
+export const KAKAO_AUTH_URL = ''
+export const NAVER_AUTH_URL = ''
+export const GOOGLE_AUTH_URL = ''
