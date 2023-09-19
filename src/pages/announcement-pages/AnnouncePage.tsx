396d4dae39f44d34e649c2b71a4c9758053f9665
HHB <bcl0206@gmail.com>
2023-09-19 09:45:08 +0900

    feat: 공지사항 페이지 구현

--- src/pages/announcement-pages/AnnouncePage.tsx
+++ src/pages/announcement-pages/AnnouncePage.tsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import DropDownSort from 'components/common/DropDownSort'
+import CategorySection from 'components/community/CategorySection'
+import AnnouncementListSection from 'components/community/announce-page/AnnouncementListSection'
+import { SORT_DROPDOWN_LIST } from 'constants/sortList'
+import { ANNOUNCE_CATEGOTY_LIST_MOCK } from 'mock/categoryData'
+import React, { useState } from 'react'
+import { styled } from 'styled-components'
 import { ShadowBox } from 'styles/reusable-style/elementStyle'
 
 const AnnouncePage = () => {
-  return <ShadowBox>AnnouncePage</ShadowBox>
+  const [selectedCategory, setSelectedCategory] = useState(ANNOUNCE_CATEGOTY_LIST_MOCK[0])
+  const [selectedSort, setSelectedSort] = useState(SORT_DROPDOWN_LIST[0])
+
+  return (
+    <ShadowBox $display="flex" $flexDirection="column" $gap="10px">
+      <StyledDiv>
+        <CategorySection
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+          categoryList={ANNOUNCE_CATEGOTY_LIST_MOCK}
+        />
+        <DropDownSort
+          selectedSort={selectedSort}
+          setSelectedSort={setSelectedSort}
+          dropdownList={SORT_DROPDOWN_LIST}
+        />
+      </StyledDiv>
+      <AnnouncementListSection
+        selectedCategory={selectedCategory}
+        selectedSort={selectedSort}
+      />
+    </ShadowBox>
+  )
 }
 
 export default AnnouncePage
+
+const StyledDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
