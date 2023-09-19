e99e26839b53ac17f6578e3d9fdf6074de417811
HHB <bcl0206@gmail.com>
2023-09-19 09:42:16 +0900

    feat: 게시판 페이지 구현

--- src/pages/board-pages/BoardPage.tsx
+++ src/pages/board-pages/BoardPage.tsx
@@ -1,32 +1,33 @@
-import CategoryBar from 'components/community/CategorySection'
-import ListSection from 'components/community/ListSection'
+import BoardListSection from 'components/community/board-page/BoardListSection'
 import React, { useState } from 'react'
 import { styled } from 'styled-components'
 import { ShadowBox } from 'styles/reusable-style/elementStyle'
-import { BOARD_SORT_DROPDOWN_LIST } from 'constants/sortList'
+import { SORT_DROPDOWN_LIST } from 'constants/sortList'
 import CategorySection from 'components/community/CategorySection'
 import DropDownSort from 'components/common/DropDownSort'
-import { CATEGOTY_LIST_MOCK } from 'mock/categoryData'
+import { BOARD_CATEGOTY_LIST_MOCK } from 'mock/categoryData'
 
 const BoardPage = () => {
-  const [selectedCategory, setSelectedCategory] = useState(CATEGOTY_LIST_MOCK[0])
-  const [selectedSort, setSelectedSort] = useState(BOARD_SORT_DROPDOWN_LIST[0])
+  const [selectedCategory, setSelectedCategory] = useState(BOARD_CATEGOTY_LIST_MOCK[0])
+  const [selectedSort, setSelectedSort] = useState(SORT_DROPDOWN_LIST[0])
 
   return (
-    <ShadowBox>
+    <ShadowBox $display="flex" $flexDirection="column" $gap="10px">
       <StyledDiv>
         <CategorySection
           selectedCategory={selectedCategory}
           setSelectedCategory={setSelectedCategory}
-          categoryList={CATEGOTY_LIST_MOCK}
+          categoryList={BOARD_CATEGOTY_LIST_MOCK}
+          useArrow
+          canCreate
         />
         <DropDownSort
           selectedSort={selectedSort}
           setSelectedSort={setSelectedSort}
-          dropdownList={BOARD_SORT_DROPDOWN_LIST}
+          dropdownList={SORT_DROPDOWN_LIST}
         />
       </StyledDiv>
-      <ListSection />
+      <BoardListSection selectedCategory={selectedCategory} selectedSort={selectedSort} />
     </ShadowBox>
   )
 }
