3daea16deff3831fc5b48508aea744f579718dd2
jieeeun2 <jieun2.dev@gmail.com>
2023-09-19 16:51:54 +0900

    feat: 로컬 회원가입 이용약관 선택 컴포넌트 구현

---
+++ src/components/auth/TermsAndConditionArea.tsx
@@ -0,0 +1,81 @@
+import { ChangeEvent, useState } from 'react'
+import { styled } from 'styled-components'
+import { AiOutlineRight } from 'react-icons/ai'
+import { Input } from 'styles/reusable-style/elementStyle'
+import { TERMS_AND_CONDITIONS_LIST } from 'constants/auth'
+
+const TermsAndConditionArea = () => {
+  const [checkList, setCheckList] = useState<number[]>([])
+
+  const checkAll = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.currentTarget.checked) {
+      setCheckList(TERMS_AND_CONDITIONS_LIST.map(({ id }) => id))
+    } else {
+      setCheckList([])
+    }
+  }
+
+  const check = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.currentTarget.checked) {
+      setCheckList([...checkList, Number(e.currentTarget.value)])
+    } else {
+      setCheckList(checkList.filter((item) => item !== Number(e.currentTarget.value)))
+    }
+  }
+
+  return (
+    <>
+      <StyledLabel>
+        <Input
+          type="checkbox"
+          checked={checkList.length === TERMS_AND_CONDITIONS_LIST.length ? true : false}
+          onChange={checkAll}
+          $width="25px"
+          $height="25px"
+        />
+        전체 동의
+      </StyledLabel>
+
+      {TERMS_AND_CONDITIONS_LIST.map(({ id, isMandatory, title, path }) => (
+        <StyledLabel key={id}>
+          <Input
+            type="checkbox"
+            value={id}
+            checked={checkList.includes(id) ? true : false}
+            onChange={check}
+            $width="25px"
+            $height="25px"
+          />
+          <StyledSpan>{isMandatory}</StyledSpan>
+          {title}
+          <StyledButton value={path}>
+            <StyledIcon />
+          </StyledButton>
+        </StyledLabel>
+      ))}
+    </>
+  )
+}
+
+export default TermsAndConditionArea
+
+const StyledLabel = styled.label`
+  display: flex;
+  gap: 10px;
+  font-weight: 700;
+  align-items: center;
+`
+const StyledButton = styled.button`
+  background: #fff;
+  border: none;
+  cursor: pointer;
+  margin-right: 0;
+  margin-left: auto;
+`
+const StyledIcon = styled(AiOutlineRight)`
+  size: 20;
+`
+
+const StyledSpan = styled.span`
+  color: #999;
+`
