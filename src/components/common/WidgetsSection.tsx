4690692244a9a03127c7900f91e3783a2258cdc1
HHB <bcl0206@gmail.com>
2023-09-19 09:30:44 +0900

    rename: 위젯 디렉토리 이름 변경 (사이드 바 -> 위젯 바)

--- src/components/common/WidgetsSection.tsx
+++ src/components/common/WidgetsSection.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import SearchBar from 'components/ui/SearchBar'
-import AnnouncementWidget from 'components/community/side-bar/AnnouncementWidget'
-import BestPostsWidget from 'components/community/side-bar/BestPostsWidget'
-import AdvertisementWidget from 'components/community/side-bar/AdvertisementWidget'
-import CommentRankWidget from 'components/community/side-bar/CommentRankWidget'
-import GatherPeopleWidget from 'components/community/side-bar/GatherPeopleWidget'
+import AnnouncementWidget from 'components/community/widget-bar/AnnouncementWidget'
+import BestPostsWidget from 'components/community/widget-bar/BestPostsWidget'
+import AdvertisementWidget from 'components/community/widget-bar/AdvertisementWidget'
+import CommentRankWidget from 'components/community/widget-bar/CommentRankWidget'
+import GatherPeopleWidget from 'components/community/widget-bar/GatherPeopleWidget'
 
 const WidgetsSection = () => {
   return (
--- src/components/community/side-bar/AdvertisementWidget.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { ShadowBox } from 'styles/reusable-style/elementStyle'
-
-const AdvertisementWidget = () => {
-  return <ShadowBox>AdvertisementWidget</ShadowBox>
-}
-
-export default AdvertisementWidget
