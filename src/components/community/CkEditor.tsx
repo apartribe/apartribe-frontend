38eb6810d59ddc7c97b955401f26b340cc06d55b
HHB <bcl0206@gmail.com>
2023-09-20 20:08:31 +0900

    feat: ck-editor 제네릭 컴포넌트화

--- src/components/community/CkEditor.tsx
+++ src/components/community/CkEditor.tsx
@@ -3,18 +3,12 @@ import { CKEditor } from '@ckeditor/ckeditor5-react'
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
 import 'styles/ckeditor.css'
 
-interface Props {
-  setInputValue: Dispatch<
-    SetStateAction<{
-      category: string
-      protected: boolean
-      title: string
-      content: string
-    }>
-  >
+interface Props<T> {
+  setInputValue: Dispatch<SetStateAction<T>>
 }
 
-class CkEditor extends Component<Props> {
+// 제네릭 컴포넌트..
+class CkEditor<T> extends Component<Props<T>> {
   render() {
     return (
       <CKEditor
@@ -22,18 +16,18 @@ class CkEditor extends Component<Props> {
         // data="<p>Hello from CKEditor&nbsp;5!</p>" // 초기값. 필요시 주석 해제
         onReady={(editor) => {
           // You can store the "editor" and use when it is needed.
-          console.log('Editor is ready to use!', editor)
+          //   console.log('Editor is ready to use!', editor)
         }}
         onChange={(event, editor) => {
           const data = editor.getData()
-          console.log({ event, editor, data })
+          //   console.log({ event, editor, data })
           this.props.setInputValue((prevState) => ({ ...prevState, content: data }))
         }}
         onBlur={(event, editor) => {
-          console.log('Blur.', editor)
+          //   console.log('Blur.', editor)
         }}
         onFocus={(event, editor) => {
-          console.log('Focus.', editor)
+          //   console.log('Focus.', editor)
         }}
       />
     )
