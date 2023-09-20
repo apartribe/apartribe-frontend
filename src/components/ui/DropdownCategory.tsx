03252671a06b14f191aa7f8e6bb1b912c4af4e4b
HHB <bcl0206@gmail.com>
2023-09-20 11:43:54 +0900

    feat: 카테고리용 드롭다운 ui 생성

---
+++ src/components/ui/DropdownCategory.tsx
@@ -0,0 +1,105 @@
+import React, { Dispatch, FC, MouseEvent, useState } from 'react'
+import { styled } from 'styled-components'
+import { IoMdArrowDropup, IoMdArrowDropdown } from 'react-icons/io'
+
+interface InputValue {
+  category: string
+  protected: boolean
+  title: string
+  content: string
+}
+
+interface Props {
+  selectedValue: InputValue
+  setSelectedValue: Dispatch<React.SetStateAction<InputValue>>
+  dropdownList: string[]
+}
+
+const DropdownCategory: FC<Props> = ({
+  selectedValue,
+  setSelectedValue,
+  dropdownList,
+}) => {
+  const changeSelectedSort = (e: MouseEvent<HTMLButtonElement>) => {
+    setSelectedValue((prevState) => ({
+      ...prevState,
+      category: e.currentTarget.innerText,
+    }))
+    setVisible(false)
+  }
+
+  const [visible, setVisible] = useState(false)
+
+  const toggleDropDown = () => {
+    setVisible((prev) => !prev)
+  }
+
+  return (
+    <StyledWrapper>
+      <StyledDiv className="view" onClick={toggleDropDown}>
+        {selectedValue.category === '' ? '게시판 선택' : selectedValue.category}
+        {visible ? <IoMdArrowDropup /> : <IoMdArrowDropdown />}
+      </StyledDiv>
+      {visible ? (
+        <StyledDiv className="select">
+          {dropdownList.slice(1).map((list, index) => (
+            <StyledButton key={index} onClick={changeSelectedSort}>
+              {list}
+            </StyledButton>
+          ))}
+        </StyledDiv>
+      ) : (
+        ''
+      )}
+    </StyledWrapper>
+  )
+}
+
+export default DropdownCategory
+
+const StyledWrapper = styled.div`
+  position: relative;
+`
+
+const StyledDiv = styled.div`
+  width: 100%;
+  color: #303030;
+  display: flex;
+  border: 1px solid #f2f2f2;
+  border-radius: 10px;
+  box-sizing: border-box;
+  background: #ffffff;
+  font-size: 15px;
+  cursor: pointer;
+  overflow: hidden;
+
+  &.view {
+    justify-content: space-between;
+    padding: 0 10px;
+    align-items: center;
+    height: 50px;
+    line-height: 40px;
+  }
+
+  &.select {
+    position: absolute;
+    top: 55px;
+    left: 0;
+    flex-direction: column;
+    height: auto;
+    z-index: 2;
+  }
+`
+
+const StyledButton = styled.button`
+  background: none;
+  border: none;
+  padding: 10px;
+  font-size: 12px;
+  cursor: pointer;
+  text-align: left;
+
+  &:hover {
+    background: #f2f2f2;
+  }
+`
