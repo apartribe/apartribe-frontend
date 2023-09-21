40cce54787adfdd0ad9b876130afba474ebe1ff3
jieeeun2 <jieun2.dev@gmail.com>
2023-09-21 18:20:31 +0900

    feat: 비밀번호 재설정 페이지

--- src/pages/auth-pages/ResetPwPage.tsx
+++ src/pages/auth-pages/ResetPwPage.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import AuthLayout from 'components/auth/AuthLayout'
+import ResetPwForm from 'components/auth/ResetPwForm'
+import ResetPwModal from 'components/auth/ResetPwModal'
+import { MouseEvent, useState } from 'react'
 
 const ResetPwPage = () => {
-  return <div>ResetPwPage</div>
+  const [modal, setModal] = useState<boolean>(false)
+
+  const resetPw = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    setModal((prev) => !prev)
+  }
+
+  //TODO: 서버응답 연결. 성공하면 navigate
+
+  return (
+    <AuthLayout>
+      <ResetPwForm resetPw={resetPw} />
+      {modal && <ResetPwModal modal={modal} />}
+    </AuthLayout>
+  )
 }
 
 export default ResetPwPage
