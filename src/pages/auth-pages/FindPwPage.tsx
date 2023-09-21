88c26ab38b41f55da146b3b464bbcfb60ee6e977
jieeeun2 <jieun2.dev@gmail.com>
2023-09-21 18:11:34 +0900

    feat: 아이디/비밀번호 찾기 개별 페이지

--- src/pages/auth-pages/FindPwPage.tsx
+++ src/pages/auth-pages/FindPwPage.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { MouseEvent, useState } from 'react'
+import AuthLayout from 'components/auth/AuthLayout'
+import FindIdPwModal from 'components/auth/FindIdPwModal'
+import FindIdPwForm from 'components/auth/FindIdPwForm'
 
 const FindPwPage = () => {
-  return <div>FindPwPage</div>
+  const [modal, setModal] = useState<boolean>(false)
+
+  const findPw = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    setModal((prev) => !prev)
+  }
+
+  return (
+    <AuthLayout>
+      <FindIdPwForm findWhat="password" find={findPw} />
+      {modal && <FindIdPwModal modal={modal} setModal={setModal} />}
+    </AuthLayout>
+  )
 }
 
 export default FindPwPage
