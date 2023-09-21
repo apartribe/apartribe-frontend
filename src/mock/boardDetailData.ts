81abf567e396056b917cad49d6e8989862a84792
HHB <bcl0206@gmail.com>
2023-09-21 16:59:33 +0900

    feat: 게시판 디테일 목 데이터 생성

---
+++ src/mock/boardDetailData.ts
@@ -0,0 +1,97 @@
+export const BOARD_DETAIL_MOCK = {
+  type: 'success',
+  issuedAt: '2023-08-20T19:31:55.350358',
+  data: {
+    id: 41,
+    avatar:
+      'https://res.cloudinary.com/dh6tdcdyj/image/upload/v1695016765/KakaoTalk_20230918_145710613_id4fua.png',
+    createdBy: '호기심 많은 탐험가',
+    title: '어제 밤에 단지 내에 비명소리 때문에 놀라신 분 있나요?',
+    category: '신혼부부 정보 공유',
+    content:
+      '<div class="wb-community-view__text"><div class="ql-editor"><h1>한국인 90%가 놓치는 하락장에 서울 내집마련 기회 잡는 법</h1><p><br></p><p>월부닷컴에 도착한 내집마련 고민 사연입니다.</p><p><strong style="background-color: rgb(255, 255, 0);"> 월급 300만원 받는 평범한 직장인도 서울 내집마련 할 수 있는지에 대해 문의</strong> 주셨어요. 사연자분께서 내집마련 어떻게 시작해야할지 모르다보니 많이 막막하셨을 거 같아요.</p><p><br></p><p>결론부터 말씀드리면,  <strong style="background-color: rgb(255, 255, 0);"> 제대로 된 방법만 알면 누구나 가능합니다!</strong></p><p><br></p><p class="ql-align-center"><img src="https://cdn.weolbu.com/data_file/ed067c95-233d-409b-9a57-c16d501ce242.png"></p><p><strong style="background-color: rgb(255, 255, 0);"><br></strong></p><p><strong style="background-color: rgb(255, 255, 0);">모아둔 돈이 적더라도 무리하게 영끌하지 않고 서울 내집마련 할 수 있는 방법!</strong></p><p>지금 바로 알려드릴게요. 딱 3가지만 기억하세요 :)</p><p><br></p><p><br></p><p><br></p><h1><strong>영끌하지 않고 안전하게 내집마련하는 3단계</strong></h1><p><br></p><p><br></p><h2>1. 예산 안의 아파트를 다 찾아보세요</h2><p><br></p><p><strong>정말 살고 싶은 아파트와 기준을 먼저 정하세요.</strong> 그리고 내가 가진 예산에 맞는 집을 수시로 찾으며 꾸준히 가격을 확인해보세요. 가격 체크를 위해선 손품과 발품을 모두 팔아야 합니다 🙂</p><p><br></p><p>먼저, <strong>네이버 부동산에 들어가서 &gt; 매매 &gt; 가격 필터링하고 내가 살고 싶은 아파트의 가격이 얼마나 되는지 체크</strong> 해보세요. 그리고 현재 소득 수준에 맞는 동네인지도 점검해보세요. 손품을 통해 살고 싶은 아파트가 추려졌다면, <strong>직접 현장 부동산을 방문하여 시세 감을 잡아보세요.</strong> 급매를 구할 수도 있거든요.</p><p><br></p><p>평소 손품, 발품을 통해 평균 가격을 알고 있어야 급매가 나왔을 때 정말 싼지 알 수 있습니다.&nbsp;</p><p><br></p><p class="ql-align-center"><img src="https://cdn.weolbu.com/data_file/8f1a8101-2202-45bf-a5f2-a0f7d18affcd.png"></p><p><br></p><p><br></p><p><br></p><h2>2. 안전한 대출로 좋은 집 고르는 비결</h2><p><br></p><p>대출을 잘 활용한다면, 내 예산보다 훨씬 좋은 집을 살 수 있습니다. 하지만 무리한 영끌은 절대 안돼요!</p><p><br></p><p><strong>영끌하지 않기 위해 매매가의 50%만 대출</strong> 하셔야 합니다. (ex. 6억 아파트라면? 3억만 대출하기)</p><p>그리고 한 달 동안 모을 수 있는 돈의 <strong>70% 정도만 원리금 상환</strong> 으로 쓰세요. 그 이상의 대출을 껴야만 살 수 있는 집이라면 아쉽더라도 과감하게 포기하셔야 합니다. 그렇지 않으면 과한 비용 지출로 나와 가족의 생활이 힘들어질 수 있어요.</p><p><br></p><p><br></p><p><br></p><h2>3. 다른 건 몰라도 서울 전세가율 60%, 꼭 기억하세요</h2><p><br></p><p class="ql-align-center"><img src="https://cdn.weolbu.com/data_file/97980b1d-d8a2-47df-9bf2-bb068e0ca97e.png"></p><p><br></p><p>서울에 살고 싶다면, 전세가율 60%가 되었을 때를 놓치지 마세요!</p><p><br></p><p>전세가율은 ‘전세가/매매가’의 비율을 의미하며 집값을 바라보는 가장 중요한 지표에요. 예를 들어, 10억짜리 집에 전세가가 7억이라면? 전세가율은 70%입니다. 전세가와 매매가 차이가 별로 나지 않는다면 내집마련하기 정말 좋은 타이밍입니다.&nbsp;</p><p><br></p><p>과거에도 부동산 하락 후 반등시점 서울 전세가율 60%일 때, 부동산 가격이 가장 바닥이었어요. 바닥을 놓쳤더라도 <strong>전세가율이 높다면</strong> 매수 적기입니다. 바닥을 잡겠다는 생각에 기회를 놓치지 마세요! <strong>과거 서울 최고 전세가율 72%</strong> 일 때도 집값은 저렴했습니다. 각 구마다 전세가율은 다르지만 서울 전세가율이 최고치일 때가 항상 매수 적기 였습니다.</p><p><br></p><p>지금 전세에 살고 있더라도 살고 싶은 아파트의 매매가와 전세가를 지속적으로 지켜보세요. 전세 만기 시점에 매매가와 전세가 차이가 적다면 매수를 고민해보세요.  <strong style="background-color: rgb(255, 255, 0);"> 꾸준히 준비하는 사람이 먼저 기회를 잡을 수 있어요.</strong></p><p><br></p><p><br></p><p class="ql-align-center"><img src="https://cdn.weolbu.com/data_file/ccb6c3e5-a97e-4d4c-a709-afdb1a3bd75c.png"></p><p><br></p><p>*서울 전세가율은 <a href="www.reb.or.kr/r-one" rel="noopener noreferrer" target="_blank"> R-one 부동산통계뷰어</a> 에서 쉽게 확인하실 수 있어요</p><p><br></p><p><br></p><p><br></p><h2>원하는 아파트에 지금 당장 가지 못해도 괜찮아요</h2><p><br></p><p class="ql-align-center"><img src="https://cdn.weolbu.com/data_file/cd57419d-97fd-41ea-acc0-4d8a165bc790.png"></p><p><br></p><p>반포 아크로리버파크 같은 꿈 같은 드림하우스… 지금 당장 못 간다고 속상해 하지 마세요. 징검다리 삼아 살고 싶은 곳(live)을 살 수(buy) 있어요! 부동산은 반드시 상승장과 하락장이 반복되는데요. 요즘 같은 하락기에 내집마련을 저렴하게 잘 해두면, 상승장에서 내집을 통한 자산 상승과 더불어 내집마련의 평안함까지 가져갈 수 있습니다.</p><p><br></p><p>하락장이라고 겁내며 모두가 떠나는 지금, 저렴하게 내집마련 할 수 있는 아주 좋은 기회입니다. 누구나 가고 싶지만 저평가 된 아파트, 실거주하면서 앞으로 자산 가격이 크게 상승할 좋은 아파트를 보는 눈을 길러보세요.&nbsp;</p><p><br></p><p><br></p><h4 class="ql-align-center"><em style="color: rgb(0, 102, 204);"> 우리가 사는 물건중에 가장 비싼 물건, 아파트</em></h4><h4 class="ql-align-center"><em style="color: rgb(0, 102, 204);"> 하물며 100만원짜리 구매도 꼼꼼히 알아보는데</em></h4><h4 class="ql-align-center"><em style="color: rgb(0, 102, 204);"> 내집마련, 부동산 꼼꼼히 알아보고 계신가요?</em></h4><p><br></p><p><br></p><h4 class="ql-align-center"><em style="color: rgb(0, 102, 204);"> 잘못된 내집마련으로 1억이 떨어지면</em></h4><h4 class="ql-align-center"><em style="color: rgb(0, 102, 204);"> 10년을 모은 종자돈이 날아갑니다.</em></h4><h4 class="ql-align-center"><em style="color: rgb(0, 102, 204);"> 잘못된 부동산 구매는 상처가 너무 큽니다.</em></h4><p><br></p><p><br></p><h4 class="ql-align-center"><em style="color: rgb(0, 102, 204);"> 당신의 현명한 부동산 구매</em></h4><h4 class="ql-align-center"><em style="color: rgb(0, 102, 204);"> 월부가 함께 돕겠습니다.</em></h4><p><br></p><p><br></p><h4 class="ql-align-center"><strong>▶ 너나위 내집마련 기초반 보러가기:</strong><a href="https://link.weolbu.com/48jB3qI" rel="noopener noreferrer" target="_blank"> https://link.weolbu.com/48jB3qI</a></h4><p class="ql-align-center"><img src="https://cdn.weolbu.com/data_file/773ce9ef-818d-4067-a5ae-07617e87a967.png"></p></div></div>',
+    liked: 4,
+    saw: 5,
+    commentCounts: 3,
+    comments: [
+      {
+        id: 3,
+        content:
+          '동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리 나라만세, 무궁화 삼천리 화려강산 대한사람 대한으로 길이 보전하세.',
+        liked: 354,
+        replyCount: 3,
+        createdAt: '2023-09-20T19:31:31.293787',
+        createdBy: 'revi1337',
+        avatar:
+          'https://res.cloudinary.com/dh6tdcdyj/image/upload/v1695016765/KakaoTalk_20230918_145710613_id4fua.png',
+        relies: [
+          {
+            id: 3,
+            content:
+              '남산 위에 저 소나무 철갑을 두른듯 바람서리 불변함은 우리 기상일세, 무궁화 삼천리 화려강산 대한사람 대한으로 길이 보전하세.',
+            liked: 13,
+            createdAt: '2023-09-20T19:31:31.293787',
+            createdBy: 'revi1337',
+            avatar:
+              'https://res.cloudinary.com/dh6tdcdyj/image/upload/v1695016765/KakaoTalk_20230918_145710613_id4fua.png',
+          },
+          {
+            id: 2,
+            content:
+              '남산 위에 저 소나무 철갑을 두른듯 바람서리 불변함은 우리 기상일세, 무궁화 삼천리 화려강산 대한사람 대한으로 길이 보전하세.',
+            liked: 0,
+            createdAt: '2023-09-20T19:31:30.048419',
+            createdBy: 'revi1337',
+            avatar:
+              'https://res.cloudinary.com/dh6tdcdyj/image/upload/v1695016765/KakaoTalk_20230918_145710613_id4fua.png',
+          },
+          {
+            id: 1,
+            content:
+              '남산 위에 저 소나무 철갑을 두른듯 바람서리 불변함은 우리 기상일세, 무궁화 삼천리 화려강산 대한사람 대한으로 길이 보전하세.',
+            liked: 5,
+            createdAt: '2023-09-20T19:30:20.522584',
+            createdBy: 'revi1337',
+            avatar:
+              'https://res.cloudinary.com/dh6tdcdyj/image/upload/v1695016765/KakaoTalk_20230918_145710613_id4fua.png',
+          },
+        ],
+      },
+      {
+        id: 2,
+        content:
+          '동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리 나라만세, 무궁화 삼천리 화려강산 대한사람 대한으로 길이 보전하세.',
+        liked: 0,
+        replyCount: 0,
+        createdAt: '2023-09-20T19:31:30.048419',
+        createdBy: 'revi1337',
+        avatar:
+          'https://res.cloudinary.com/dh6tdcdyj/image/upload/v1695016765/KakaoTalk_20230918_145710613_id4fua.png',
+        relies: [],
+      },
+      {
+        id: 1,
+        content:
+          '동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리 나라만세, 무궁화 삼천리 화려강산 대한사람 대한으로 길이 보전하세.',
+        liked: 72,
+        replyCount: 1,
+        createdAt: '2023-09-20T19:30:20.522584',
+        createdBy: 'revi1337',
+        avatar:
+          'https://res.cloudinary.com/dh6tdcdyj/image/upload/v1695016765/KakaoTalk_20230918_145710613_id4fua.png',
+        relies: [
+          {
+            id: 3,
+            content:
+              '남산 위에 저 소나무 철갑을 두른듯 바람서리 불변함은 우리 기상일세, 무궁화 삼천리 화려강산 대한사람 대한으로 길이 보전하세.',
+            liked: 13,
+            createdAt: '2023-09-20T19:31:31.293787',
+            createdBy: 'revi1337',
+            avatar:
+              'https://res.cloudinary.com/dh6tdcdyj/image/upload/v1695016765/KakaoTalk_20230918_145710613_id4fua.png',
+          },
+        ],
+      },
+    ],
+  },
+}
