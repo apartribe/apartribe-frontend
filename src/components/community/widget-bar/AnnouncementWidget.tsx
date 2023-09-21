8c232cd71599ebad4ee29a03b3e9436404f1c6a8
HHB <bcl0206@gmail.com>
2023-09-21 22:03:17 +0900

    feat: 공지사항 위젯 구현

--- src/components/community/widget-bar/AnnouncementWidget.tsx
+++ src/components/community/widget-bar/AnnouncementWidget.tsx
@@ -1,7 +1,9 @@
 import WidgetTitleArea from 'components/common/WidgetTitleArea'
 import React from 'react'
-import { ShadowBox } from 'styles/reusable-style/elementStyle'
+import { Badge, P, ShadowBox } from 'styles/reusable-style/elementStyle'
 import { HiSpeakerphone } from 'react-icons/hi'
+import { ANNONCEMENT_MOCK } from 'mock/announcementData'
+import { styled } from 'styled-components'
 
 const AnnouncementWidget = () => {
   return (
@@ -12,9 +14,21 @@ const AnnouncementWidget = () => {
         hasSeeMore={true}
         seeMorePath="/community/123/announcements"
       />
-      AnnouncementWidget
+      {ANNONCEMENT_MOCK.slice(0, 1).map((item, index) => (
+        <StyledDiv key={index}>
+          <Badge $width="50px">{item.urgency}</Badge>
+          <P>{item.title}</P>
+        </StyledDiv>
+      ))}
     </ShadowBox>
   )
 }
 
 export default AnnouncementWidget
+
+const StyledDiv = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  padding-left: 10px;
+`
