b8fb6d4267185df75196d0f4dac1bd3a123a1edb
jieeeun2 <jieun2.dev@gmail.com>
2023-09-27 17:06:14 +0900

    feat: 변수명, 잘못된 옵션 수정 및 logout, reissueToken 추가

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,60 +1,91 @@
-import { AxiosPromise } from 'axios'
-import { ConfirmEmailTokenResult, ConfirmNicknameResult, InputValue } from 'types/auth'
+import { AxiosPromise, AxiosResponse } from 'axios'
+import {
+  ConfirmEmailTokenResult,
+  ConfirmNicknameResult,
+  SignupInputValue,
+} from 'types/auth'
 
 export class AuthService {
   #httpClient
-  #tokenRepository
+  #webStorage
 
-  constructor(httpClient: any, tokenRepository: any) {
+  constructor(httpClient: any, webStorage: any) {
     this.#httpClient = httpClient
-    this.#tokenRepository = tokenRepository
+    this.#webStorage = webStorage
   }
 
-  async sendEmailAuthCode(email: string) {
-    console.log('히히')
-    const test = await this.#httpClient.axiosInstance('/api/auth/email/send', {
+  sendEmail(email: string) {
+    this.#httpClient.axiosInstance('/api/auth/email/send', {
       method: 'GET',
       params: { email: email },
     })
-    console.log(test, test.data)
   }
 
-  confirmEmailToken(email: string, token: string): AxiosPromise<ConfirmEmailTokenResult> {
-    return this.#httpClient.axiosInstance('/api/auth/email/confirm', {
-      method: 'GET',
-      params: {
-        email: email,
-        token: token,
-      },
-    })
+  confirmEmail(
+    email: string,
+    token: string,
+  ) /* : AxiosPromise<ConfirmEmailTokenResult> */ {
+    this.#httpClient
+      .axiosInstance('/api/auth/email/confirm', {
+        method: 'GET',
+        params: {
+          email: email,
+          token: token,
+        },
+      })
+      .then((response: AxiosResponse) => console.log('==confirmEmail==', response))
   }
 
-  confirmNickname(nickname: string): AxiosPromise<ConfirmNicknameResult> {
-    return this.#httpClient.axiosInstance('/api/auth/member/check', {
-      method: 'GET',
-      params: { nickname: nickname },
-    })
+  checkNickname(nickname: string) /* : AxiosPromise<ConfirmNicknameResult> */ {
+    this.#httpClient
+      .axiosInstance('/api/auth/member/check', {
+        method: 'GET',
+        params: { nickname: nickname },
+      })
+      .then((response: AxiosResponse) => console.log('==checkNickname==', response))
   }
 
-  signup(body: InputValue): AxiosPromise {
-    return this.#httpClient.axiosInstance('/api/auth/join', {
-      method: 'POST',
-      body,
-    })
+  signup(body: SignupInputValue) /* : AxiosPromise */ {
+    console.log(body)
+    this.#httpClient
+      .axiosInstance('/api/auth/join', {
+        method: 'POST',
+        data: body,
+      })
+      .then((response: AxiosResponse) => console.log('==signup==', response))
   }
 
-  signin(email: string, password: string): AxiosPromise {
-    return this.#httpClient.axiosInstance('/api/auth/login', {
-      method: 'POST',
-      body: {
-        email: email,
-        password: password,
-      },
-    })
+  signin(email: string, password: string) /* : AxiosPromise  */ {
+    this.#httpClient
+      .axiosInstance('/api/auth/login', {
+        method: 'POST',
+        data: {
+          email: email,
+          password: password,
+        },
+      })
+      .then((response: AxiosResponse) => {
+        console.log('==signin response.headers==', response.headers)
+        console.log('AccessToken: ', response.headers['authorization'].substring(7))
+        console.log('document.cookie: ', document.cookie)
+        this.#webStorage.set(response.headers['authorization'].substring(7))
+      })
   }
 
-  reissueToken(): AxiosPromise {
-    return this.#httpClient.axiosInstance('/api/reissue/token', {
+  logout() {
+    this.#webStorage.remove()
+    //TODO: 로그아웃할때 쿠키도 삭제해주기
+  }
+
+  /* 
+    HttpClient에서 AuthService를 부르는게 이상해서 일단 이 reissueToken()을 호출안하고 
+    this.axiosInstance('/api/reissue/token', {
+      method: 'POST'
+    }) 
+    이렇게 바로 불러다가 쓰게했는데 이게맞나...
+  */
+  reissueToken() /* : AxiosPromise */ {
+    this.#httpClient.axiosInstance('/api/reissue/token', {
       method: 'POST',
     })
   }
