5c2ec968448baba6ff6534b71f95a15646f53757
ChoongLim <bcl0206@naver.com>
2023-10-07 13:35:35 +0900

    feat: 게시물 조회 실 데이터 연결

--- src/pages/announcement-pages/DetailAnnouncePage.tsx
+++ src/pages/announcement-pages/DetailAnnouncePage.tsx
@@ -1,23 +1,38 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { ShadowBox } from 'styles/reusable-style/elementStyle'
 import parse from 'html-react-parser'
 import { styled } from 'styled-components'
 import DetailHeaderSection from 'components/community/DetailHeaderSection'
 import DetailCommentSection from 'components/community/DetailCommentSection'
 import { ANNOUNCEMENT_DETAIL_MOCK } from 'mock/announcementDetailData'
+import { useDispatch, useSelector } from 'react-redux'
+import { getPost } from 'redux/post/actions'
 
 const DetailAnnouncePage = () => {
-  const {
-    issuedAt,
-    data,
-    data: { content, commentCounts, comments },
-  } = ANNOUNCEMENT_DETAIL_MOCK
+  // const {
+  //   issuedAt,
+  //   data,
+  //   // data: { content, commentCounts, comments },
+  // } = ANNOUNCEMENT_DETAIL_MOCK
+
+  const { data, loading, error } = useSelector((state: any) => state.post.post)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch<any>(
+      getPost({ boardType: 'article' /* , category: '자유게시판' */, page: 1 }),
+    )
+  }, [dispatch])
+
+  if (loading) return <div>로딩중...</div>
+  if (error) return <div>에러 발생!</div>
+  if (!data) return null
 
   return (
     <ShadowBox $padding="30px">
-      <DetailHeaderSection issuedAt={issuedAt} data={data} />
-      <DetailHtmlSection>{parse(content)}</DetailHtmlSection>
-      <DetailCommentSection commentCounts={commentCounts} comments={comments} />
+      <DetailHeaderSection data={data} />
+      <DetailHtmlSection>{parse(data.data.content)}</DetailHtmlSection>
+      {/* <DetailCommentSection commentCounts={commentCounts} comments={comments} /> */}
     </ShadowBox>
   )
 }
--- src/pages/gather-people-pages/DetailGatherPeoplePage.tsx
+++ src/pages/gather-people-pages/DetailGatherPeoplePage.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { ShadowBox } from 'styles/reusable-style/elementStyle'
 import parse from 'html-react-parser'
 import { styled } from 'styled-components'
@@ -6,20 +6,35 @@ import DetailHeaderSection from 'components/community/DetailHeaderSection'
 import DetailCommentSection from 'components/community/DetailCommentSection'
 import DetailInfoSection from 'components/community/DetailInfoSection'
 import { GATHER_PEOPLE_DETAIL_MOCK } from 'mock/gatherPeopleDetailData'
+import { useDispatch, useSelector } from 'react-redux'
+import { getPost } from 'redux/post/actions'
 
 const DetailGatherPeoplePage = () => {
-  const {
-    issuedAt,
-    data,
-    data: { content, commentCounts, comments },
-  } = GATHER_PEOPLE_DETAIL_MOCK
+  // const {
+  //   issuedAt,
+  //   data,
+  //   data: { content, commentCounts, comments },
+  // } = GATHER_PEOPLE_DETAIL_MOCK
+
+  const { data, loading, error } = useSelector((state: any) => state.post.post)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch<any>(
+      getPost({ boardType: 'article' /* , category: '자유게시판' */, page: 1 }),
+    )
+  }, [dispatch])
+
+  if (loading) return <div>로딩중...</div>
+  if (error) return <div>에러 발생!</div>
+  if (!data) return null
 
   return (
     <ShadowBox $padding="30px">
-      <DetailHeaderSection issuedAt={issuedAt} data={data} />
+      <DetailHeaderSection data={data} />
       <DetailInfoSection data={data} />
-      <DetailHtmlSection>{parse(content)}</DetailHtmlSection>
-      <DetailCommentSection commentCounts={commentCounts} comments={comments} />
+      <DetailHtmlSection>{parse(data.data.content)}</DetailHtmlSection>
+      {/* <DetailCommentSection commentCounts={commentCounts} comments={comments} /> */}
     </ShadowBox>
   )
 }
