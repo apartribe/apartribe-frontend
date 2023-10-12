b9a6b397c4bdf806d41113ac8becca9e37d59637
ChoongLim <bcl0206@naver.com>
2023-10-13 08:00:42 +0900

    remove: 리덕스 전략 변경으로 인한 삭제

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,14 +1,6 @@
 import { combineReducers } from 'redux'
-import { postReducer } from './post/reducer'
 import { postsReducer } from './posts/reducer'
-import { commentsReducer } from './comments/reducer'
-import { commentReducer } from './comment/reducer'
-import { replyReducer } from './reply/reducer'
 
 export const rootReducer = combineReducers({
   posts: postsReducer,
-  post: postReducer,
-  comments: commentsReducer,
-  comment: commentReducer,
-  reply: replyReducer,
 })
