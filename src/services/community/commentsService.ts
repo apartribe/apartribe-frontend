1cd8ff18f2f9658a79d6aa6615f1c394a5ea982b
ChoongLim <bcl0206@naver.com>
2023-10-13 13:12:06 +0900

    feat: comments 서비스 구현 (R)

--- src/services/community/commentsService.ts
+++ src/services/community/commentsService.ts
@@ -1,1 +1,30 @@
-export {}
+// http://{{host}}/api/board/2/comment?size=4&page=1
+
+import { AxiosResponse } from 'axios'
+import { CommentsData } from 'components/community/DetailCommentSection'
+import { instance } from 'configs/axios'
+
+interface GetParam {
+  postId: string
+  page: number
+}
+
+export interface Comments {
+  data: CommentsData
+  issuedAt: string
+  type: string
+}
+
+export const commentsService = {
+  async getComments(param: GetParam): Promise<Comments | undefined> {
+    const { postId, page } = param
+    try {
+      const response: AxiosResponse = await instance(
+        `/api/board/${postId}/comment?size=10&page=${page}`,
+      )
+      return response.data
+    } catch (error) {
+      console.error(error)
+    }
+  },
+}
