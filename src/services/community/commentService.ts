9d3504cc0f07b7fd5a7fec4d81e79c6c8ea4912c
ChoongLim <bcl0206@naver.com>
2023-10-13 13:13:21 +0900

    feat: comment 서비스 구현 (C)

--- src/services/community/commentService.ts
+++ src/services/community/commentService.ts
@@ -1,1 +1,25 @@
-export {}
+import { instance } from 'configs/axios'
+
+interface AddParam {
+  postId: string
+  content: string
+}
+
+export const commentService = {
+  async addComment(param: AddParam) {
+    const { postId, content } = param
+    try {
+      const response = await instance(`/api/board/${postId}/comment`, {
+        method: 'post',
+        data: {
+          content,
+        },
+      })
+      return response.data
+    } catch (error) {
+      console.error(error)
+    }
+  },
+  // updateComment(){},
+  // deleteComment(){}
+}
