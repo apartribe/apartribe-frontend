f1cceb6c42a77efb1e52b3aa117e9720c5da37fc
ChoongLim <bcl0206@naver.com>
2023-10-13 13:21:41 +0900

    feat: thunk 생성 로직 타입 변경

--- src/utils/redux-async-utils/handleAsyncActions.ts
+++ src/utils/redux-async-utils/handleAsyncActions.ts
@@ -1,10 +1,11 @@
-//TODO : 타입 모두 수정
-
 import { reducerUtils } from './reducerUtils'
+import { Action } from 'redux/posts/reducer'
 
 export const handleAsyncActions = (type: string, key: string) => {
   const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`]
-  return (state: any, action: any) => {
+  return (state: any, action: Action) => {
+    //TODO : 타입 수정 요망
+
     switch (action.type) {
       case type:
         return {
