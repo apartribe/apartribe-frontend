85346870415b5a53ec7da5ac4d15a85431ec519e
HHB <bcl0206@gmail.com>
2023-10-13 13:01:06 +0900

    feat: 코드 재사용성을 위한 경로 변경

--- src/constants/fixedButtonList.ts
+++ src/constants/fixedButtonList.ts
@@ -1,6 +1,6 @@
 export const FIXED_BUTTON_LISTS = [
   // 추후 아이디 부분 변경 필요
-  { option: '공지사항 작성', path: '/community/123/announcements/add' },
-  { option: '같이 하실 분 작성', path: '/community/123/gather-people/add' },
-  { option: '일반 게시물 작성', path: '/community/123/bbs/add' },
+  { option: '공지사항 작성', path: '/community/123/announce/add' },
+  { option: '같이 하실 분 작성', path: '/community/123/together/add' },
+  { option: '일반 게시물 작성', path: '/community/123/article/add' },
 ]
--- src/constants/navList.ts
+++ src/constants/navList.ts
@@ -8,6 +8,10 @@ export const LANDING_NAV_LIST = [
 // 경로 수정 필요
 export const COMMUNITY_NAV_LIST = [
   { title: '커뮤니티 홈', path: '/community/123' },
-  { title: '아파트 공지사항', path: '/community/123/announcements' },
-  { title: '같이 하실 분', path: '/community/123/gather-people' },
+  { title: '아파트 공지사항', path: '/community/123/announce' },
+  { title: '같이 하실 분', path: '/community/123/together' },
 ]
+
+// TODO: 빼먹은 경로들 다 여기에 추가해서 관리할 것.
+// 파일명 포괄적으로 바꾸고 FIXED_BUTTON_LISTS 도 여기에 통합할 것.
+// 라우터에 있는 것도 여기서 활용하면 좋을 듯.
