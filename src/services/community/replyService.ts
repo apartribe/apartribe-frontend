dffa9faffbe3aa8840272e0bbc9af948b6fbe807
HHB <bcl0206@gmail.com>
2023-10-13 13:15:34 +0900

    feat: reply 서비스 구현 (C)

--- src/services/community/replyService.ts
+++ src/services/community/replyService.ts
@@ -1,1 +1,27 @@
-export {}
+import { instance } from 'configs/axios'
+
+interface AddParam {
+  postId: string
+  parentId: number
+  content: string
+}
+
+export const replyService = {
+  async addReply(param: AddParam) {
+    const { postId, parentId, content } = param
+    try {
+      const response = await instance(`/api/board/${postId}/comment`, {
+        method: 'post',
+        data: {
+          parentId,
+          content,
+        },
+      })
+      return response.data
+    } catch (error) {
+      console.error(error)
+    }
+  },
+  // updateComment(){},
+  // deleteComment(){}
+}
