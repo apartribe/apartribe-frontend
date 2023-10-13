c355f630fad1e42e013811c9b94b62f4abd48735
ChoongLim <bcl0206@naver.com>
2023-10-13 13:08:25 +0900

    feat: posts 서비스 구현

--- src/services/community/postsService.ts
+++ src/services/community/postsService.ts
@@ -1,31 +1,71 @@
 import { AxiosResponse } from 'axios'
 import { instance } from 'configs/axios'
+import { AnnouncementMockType } from 'mock/announcementData'
+import { BoardMockType } from 'mock/boardData'
+import { GatherPeopleMockType } from 'mock/gatherPeopleData'
 
-export type BoardType = 'article' | 'announce' | 'gether-people'
+export type BoardType = 'article' | 'announce' | 'together'
 
 interface Param {
   boardType: BoardType
   category: string
+  sort: string
   page: number
 }
 
+export interface Posts {
+  data: BoardMockType | AnnouncementMockType | GatherPeopleMockType
+  issuedAt: string
+  type: string
+}
+
 export const postsService = {
-  async getPosts(param: Param): Promise<any> {
+  async getPosts(param: Param): Promise<Posts | undefined> {
     // TDOD: 타입 수정
-    const { boardType, category, page } = param
+    const { boardType, category /* , sort */, page } = param
+    let response: AxiosResponse
     try {
-      const response: AxiosResponse = await instance(`/api/${boardType}`, {
-        method: 'get',
-        params: {
-          size: 10,
-          category,
-          page,
-        },
-      })
-      return response.data
+      const mappedCategory = category === '전체' ? '' : category
+      switch (boardType) {
+        case 'article':
+          response = await instance(`/api/${boardType}`, {
+            method: 'get',
+            params: {
+              size: 10,
+              category: mappedCategory,
+              //sort,
+              page,
+            },
+          })
+
+          return response.data
+        case 'announce':
+          response = await instance(`/api/${boardType}`, {
+            method: 'get',
+            params: {
+              size: 10,
+              level: mappedCategory,
+              //sort,
+              page,
+            },
+          })
+          return response.data
+        case 'together':
+          response = await instance(`/api/${boardType}`, {
+            method: 'get',
+            params: {
+              size: 10,
+              category: mappedCategory,
+              //sort,
+              page,
+            },
+          })
+          return response.data
+        default:
+          null
+      }
     } catch (error) {
       console.error(error)
     }
   },
-  // getPostsCount,
 }
