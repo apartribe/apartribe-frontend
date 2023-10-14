fc8d1cc583391f79f252343e8abff8495f9ba89a
ChoongLim <bcl0206@naver.com>
2023-10-14 12:54:40 +0900

    feat: reIssue 인터벌 로직 구현

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import Router from 'routes/Router'
 import { Provider } from 'react-redux'
 import { store } from 'redux/store'
+import { auth } from 'services/auth'
 
 const root = createRoot(document.getElementById('root') as HTMLElement)
+
+const reIssue = () => {
+  auth.reissueToken()
+  const timer = setInterval(() => {
+    auth.reissueToken()
+  }, 70000)
+
+  return () => {
+    clearInterval(timer)
+  }
+}
+
+reIssue()
 
 root.render(
   <React.StrictMode>
