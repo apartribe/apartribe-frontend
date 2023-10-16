36faaaa558983b71bde7e6f6deb45391c0cc09f3
HHB <bcl0206@gmail.com>
2023-10-16 21:46:54 +0900

    feat: 구인 공고 서비스 별도 구현

---
+++ src/services/community/togetherService.ts
@@ -0,0 +1,169 @@
+import axios, { AxiosResponse } from 'axios'
+import { BoardType } from './postsService'
+import { instance } from 'configs/axios'
+import { AddTogetherType, UpdateTogetherType } from 'types/community-type/togetherType'
+import dateformat from 'utils/dateFormat'
+
+interface addParam {
+  boardType: BoardType
+  data: AddTogetherType
+}
+
+interface getParam {
+  boardType: BoardType
+  aptId: string
+  postId: string
+}
+
+interface updateParam {
+  boardType: BoardType
+  data: UpdateTogetherType
+  postId: string
+}
+
+interface deleteParam {
+  boardType: BoardType
+  postId: string
+}
+
+export const togetherService = {
+  async addPost(param: addParam) {
+    const { boardType, data } = param
+    const {
+      category,
+      title,
+      content,
+      thumbnail,
+      description,
+      recruitFrom,
+      recruitTo,
+      meetTime,
+      target,
+      location,
+      contributeStatus,
+    } = data
+    console.log('뭘까 400에러', data)
+
+    try {
+      await instance(`/api/${boardType}`, {
+        method: 'post',
+        data: {
+          category,
+          title,
+          content,
+          thumbnail,
+          description,
+          recruitFrom: dateformat(recruitFrom.toString()),
+          recruitTo: dateformat(recruitTo.toString()),
+          meetTime,
+          target,
+          location,
+          contributeStatus,
+        },
+      })
+      return {
+        statusCode: 201,
+        message: '게시물이 등록되었습니다. 커뮤니티로 이동합니다.',
+      }
+    } catch (error) {
+      console.error(error)
+      if (axios.isAxiosError(error)) {
+        return {
+          statusCode: 500,
+          message: '게시물 등록에 실패하였습니다. 다시 시도해주세요.',
+        }
+      } else {
+        throw new Error('different error than axios')
+      }
+    }
+  },
+
+  async getPost(param: getParam) {
+    const { boardType /* , aptId */, postId } = param
+    try {
+      const response: AxiosResponse = await instance(`/api/${boardType}/${postId}`, {
+        // TODO: 아파트 아이디 추가 필요
+        method: 'get',
+      })
+
+      return response.data
+    } catch (error) {
+      console.error(error)
+    }
+  },
+
+  async updatePost(param: updateParam) {
+    const { boardType, data, postId } = param
+    const {
+      category,
+      title,
+      content,
+      thumbnail,
+      description,
+      recruitFrom,
+      recruitTo,
+      meetTime,
+      target,
+      location,
+      contributeStatus,
+      recruitStatus,
+    } = data
+
+    try {
+      await instance(`/api/${boardType}/${postId}`, {
+        method: 'put',
+        data: {
+          category,
+          title,
+          content,
+          thumbnail,
+          description,
+          recruitFrom: dateformat(recruitFrom.toString()),
+          recruitTo: dateformat(recruitTo.toString()),
+          meetTime,
+          target,
+          location,
+          contributeStatus,
+          recruitStatus,
+        },
+      })
+      return {
+        statusCode: 201,
+        message: '게시물이 수정되었습니다. 수정된 게시물로 이동합니다.',
+      }
+    } catch (error) {
+      console.error(error)
+      if (axios.isAxiosError(error)) {
+        return {
+          statusCode: 500,
+          message: '게시물 수정에 실패하였습니다. 다시 시도해주세요.',
+        }
+      } else {
+        throw new Error('different error than axios')
+      }
+    }
+  },
+
+  async deletePost(param: deleteParam) {
+    const { boardType, postId } = param
+    try {
+      await instance(`/api/${boardType}?${boardType}Id=${postId}`, {
+        method: 'delete',
+      })
+      return {
+        statusCode: 204,
+        message: '게시물이 삭제되었습니다. 커뮤니티로 이동합니다.',
+      }
+    } catch (error) {
+      console.error(error)
+      if (axios.isAxiosError(error)) {
+        return {
+          statusCode: 500,
+          message: '게시물 삭제에 실패하였습니다. 다시 시도해주세요.',
+        }
+      } else {
+        throw new Error('different error than axios')
+      }
+    }
+  },
+}
