6c757c9075ebe3d9446c9a3ebb0ad635391bce27
HHB <bcl0206@gmail.com>
2023-10-16 22:03:32 +0900

    feat: 공지사항 카드 실데이터 연결

--- src/components/community/announce-page/AnnouncementCard.tsx
+++ src/components/community/announce-page/AnnouncementCard.tsx
@@ -3,70 +3,87 @@ import { Badge, Img, P } from 'styles/reusable-style/elementStyle'
 import { AiOutlineEye, AiOutlineLike } from 'react-icons/ai'
 import { BiConversation } from 'react-icons/bi'
 import { styled } from 'styled-components'
-import { AnnouncementMockType } from 'mock/announcementData'
 import { useNavigate } from 'react-router-dom'
+import { AnnounceCardType } from 'types/community-type/announceType'
+import { BoardType } from 'services/community/postsService'
+import { timeAgo } from 'utils/timeAgo'
 
 interface Props {
-  post: AnnouncementMockType
+  boardType: BoardType
+  post: AnnounceCardType
 }
 
-const badgeColor = (urgency: string): string => {
-  if (urgency === '일반') return '#0B2A08'
-  if (urgency === '긴급') return '#C9AB0C'
+// level 과 category 관련 이슈 announceType.ts 주석 참고.
+const badgeColor = (category: string | undefined): string => {
+  if (category === '일반') return '#0B2A08'
+  if (category === '긴급') return '#C9AB0C'
   return '#EA1616'
 }
 
 const AnnouncementCard: FC<Props> = ({
-  post: { avatar, job, writer, date, urgency, title, content, view, like, comment, url },
+  post: {
+    id,
+    level: category,
+    title,
+    content,
+    createdBy,
+    createdAt,
+    saw,
+    liked,
+    commentCounts,
+    thumbnail,
+  },
 }) => {
   const navigate = useNavigate()
 
   const moveToDetail = () => {
-    navigate('/community/123/announcements/45/detail') // 추후 경로 수정
+    navigate(`/community/123/announce/${id}/detail`) // 추후 경로 수정
   }
 
   return (
     <StyledWrapper className="flex" onClick={moveToDetail}>
       <Img
-        src={avatar}
+        src="https://res.cloudinary.com/dh6tdcdyj/image/upload/v1695016765/KakaoTalk_20230918_145710613_id4fua.png"
         $width="40px"
         $height="40px"
         $borderRadius="50%"
         $lineHeight="12px"
         $margin="5px 0"
       />
-      <StyledDiv className="column">
-        <div>
-          <P $fontSize="12px" $fontWeight="700">
-            [{job}] {writer}
-          </P>
-          <P $fontSize="10px" $color="#b3b3b3" $lineHeight="12px">
-            {date}
-          </P>
-        </div>
-        <StyledDiv className="row">
-          <Badge $background={badgeColor(urgency)}>{urgency}</Badge>
-          <StyledParagraph className="singleLineEclips">{title}</StyledParagraph>
-        </StyledDiv>
-        <StyledParagraph className="doubleLineEclips">{content}</StyledParagraph>
-        <StyledDiv className="row">
-          <P $fontSize="12px">
-            <AiOutlineEye />
-            &nbsp;{view}
-          </P>
-          <P $fontSize="12px">
-            <AiOutlineLike />
-            &nbsp;{like}
-          </P>
-          <P $fontSize="12px">
-            <BiConversation />
-            &nbsp;{comment}
-          </P>
+      <StyledDiv className="row">
+        <StyledDiv className="column">
+          <div>
+            <P $fontSize="12px" $fontWeight="700">
+              [직책정보필요] {createdBy}
+            </P>
+            <P $fontSize="10px" $color="#b3b3b3" $lineHeight="12px">
+              {timeAgo(createdAt)}
+            </P>
+          </div>
+          <StyledDiv className="row">
+            <Badge $background={badgeColor(category)}>{category}</Badge>
+            <StyledParagraph className="singleLineEclips">{title}</StyledParagraph>
+          </StyledDiv>
+          <StyledParagraph className="doubleLineEclips">{content}</StyledParagraph>
+          <StyledDiv className="row">
+            <P $fontSize="12px">
+              <AiOutlineEye />
+              &nbsp;{saw}
+            </P>
+            <P $fontSize="12px">
+              <AiOutlineLike />
+              &nbsp;{liked}
+            </P>
+            <P $fontSize="12px">
+              <BiConversation />
+              &nbsp;{commentCounts}
+            </P>
+          </StyledDiv>
         </StyledDiv>
       </StyledDiv>
-      {url ? (
+      {thumbnail ? (
         <Img
-          src={url}
+          src={thumbnail}
           $width="150px"
           $height="80px"
           $borderRadius="5px"
@@ -83,6 +100,7 @@ export default AnnouncementCard
 
 const StyledWrapper = styled.div`
   max-width: 900px;
+  width: 100%;
   height: 170px;
   display: flex;
   border-top: 1px solid #f2f2f2;
@@ -94,6 +112,7 @@ const StyledWrapper = styled.div`
 const StyledDiv = styled.div`
   &.row {
     display: flex;
+    flex: 1;
     flex-direction: row;
     align-items: center;
     gap: 5px;
