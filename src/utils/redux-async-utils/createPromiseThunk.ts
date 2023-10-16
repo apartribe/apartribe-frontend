8a198696e440bcbec421ed19f2ef9a86d16513fe
HHB <bcl0206@gmail.com>
2023-10-16 21:38:29 +0900

    feat: 리덕스 사용 보류로 인한 주석처리

--- src/utils/redux-async-utils/createPromiseThunk.ts
+++ src/utils/redux-async-utils/createPromiseThunk.ts
@@ -1,27 +1,29 @@
 // TODO : 타입 모두 수정
-import { Dispatch } from 'redux'
-import { BoardType, Posts } from 'services/community/postsService'
+// import { Dispatch } from 'redux'
+// import { BoardType, Posts } from 'services/community/postsService'
 
-interface Param {
-  boardType: BoardType
-  category: string
-  page: number
-  sort: string
-}
+// interface Param {
+//   boardType: BoardType
+//   category: string
+//   page: number
+//   sort: string
+// }
 
-export const createPromiseThunk = (
-  type: string,
-  promiseCreater: (param: Param) => Promise<Posts | undefined>,
-) => {
-  const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`]
+// export const createPromiseThunk = (
+//   type: string,
+//   promiseCreater: (param: Param) => Promise<Posts | undefined>,
+// ) => {
+//   const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`]
 
-  return (param: Param) => async (dispatch: Dispatch) => {
-    dispatch({ type, param })
-    try {
-      const payload = await promiseCreater(param)
-      dispatch({ type: SUCCESS, payload })
-    } catch (error) {
-      dispatch({ type: ERROR, payload: error, error: true })
-    }
-  }
-}
+//   return (param: Param) => async (dispatch: Dispatch) => {
+//     dispatch({ type, param })
+//     try {
+//       const payload = await promiseCreater(param)
+//       dispatch({ type: SUCCESS, payload })
+//     } catch (error) {
+//       dispatch({ type: ERROR, payload: error, error: true })
+//     }
+//   }
+// }
+
+export {}
