7076e3712d6b05d116f74ce581ffe096d1950ceb
HHB <bcl0206@gmail.com>
2023-10-21 12:36:46 +0900

    feat: 메인 배너 섹션 케러셀 적용

--- src/components/landing/BannerSection.tsx
+++ src/components/landing/BannerSection.tsx
@@ -1,97 +1,41 @@
 import React from 'react'
-import { styled } from 'styled-components'
-import { LogoMainWhite } from 'assets/logos'
-import AptSearchBar from 'components/common/apt-sugget-search-bar/AptSearchBar'
-import { BANNER_TEXT } from 'constants/landing/banner'
 import { Container, Inner } from 'styles/reusable-style/layoutStyle'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import Slider from 'react-slick'
+import BannerFirst from './BannerFirst'
+import BannerSecond from './BannerSecond'
+import BannerThird from './BannerThird'
 
 const BannerSection = () => {
-  const { title, question, explain, doIt } = BANNER_TEXT
+  const settings = {
+    dots: false,
+    fade: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+  }
 
   return (
     <Container $background="#FFFFFF">
       <Inner>
-        <StyledWrapper>
-          <StyledDiv className="left">
-            <StyledParagraph className="sm">{title}</StyledParagraph>
-            <StyledParagraph className="xl">
-              {question[0]}
-              <br />
-              {question[1]}
-            </StyledParagraph>
-            <StyledParagraph className="sm">
-              {explain[0]}
-              <br />
-              {explain[1]}
-            </StyledParagraph>
-            <StyledParagraph className="lg">{doIt}</StyledParagraph>
-            <AptSearchBar />
-          </StyledDiv>
-          <StyledDiv className="right">
-            <LogoMainWhite width="350px" height="400px" />
-          </StyledDiv>
-        </StyledWrapper>
+        <Slider {...settings}>
+          <div>
+            <BannerFirst />
+          </div>
+          <div>
+            <BannerSecond />
+          </div>
+          <div>
+            <BannerThird />
+          </div>
+        </Slider>
       </Inner>
     </Container>
   )
 }
 
 export default BannerSection
-
-const StyledWrapper = styled.div`
-  background: linear-gradient(
-    330deg,
-    rgba(26, 42, 58, 1) 0%,
-    rgba(56, 71, 81, 1) 75%,
-    rgba(75, 89, 99, 1) 84%,
-    rgba(61, 75, 85, 1) 92%,
-    rgba(26, 42, 58, 1) 100%
-  );
-  width: 100%;
-  height: 450px;
-  border-radius: 20px;
-  display: flex;
-  margin-bottom: 20px;
-  color: #ffffff;
-`
-
-const StyledDiv = styled.div`
-  display: flex;
-
-  &.left {
-    width: 50%;
-    flex-direction: column;
-    justify-content: center;
-    gap: 10px;
-    margin-left: 100px;
-  }
-
-  &.right {
-    width: 50%;
-    justify-content: center;
-    align-items: center;
-  }
-`
-
-const StyledParagraph = styled.p`
-  margin: 0px;
-
-  &.sm {
-    font-size: 12px;
-    display: flex;
-    gap: 5px;
-    align-items: center;
-    color: #c8c8c8;
-    line-height: 25px;
-  }
-
-  &.lg {
-    font-size: 20px;
-    margin-top: 40px;
-  }
-
-  &.xl {
-    font-size: 30px;
-    font-weight: 900;
-  }
-`
