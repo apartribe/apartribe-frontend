5fa11324d0fac554c15bbe53375a5a15e1c36153
ChoongLim <bcl0206@naver.com>
2023-10-21 10:51:07 +0900

    feat: 목데이터 내용 변경

--- src/mock/announcementData.ts
+++ src/mock/announcementData.ts
@@ -17,7 +17,7 @@ export const ANNONCEMENT_MOCK = [
   {
     avatar:
       'https://res.cloudinary.com/dh6tdcdyj/image/upload/v1695016765/KakaoTalk_20230918_145710613_id4fua.png',
-    title: '9월 23일 (수) 단수 예정입니다.',
+    title: '지하주차장 누수로 인한 이동주차 요망',
     content:
       '어제 밤 한시쯤에 아파트 정문 부근에서 여자 비명소리 들으신 분 있나요? 너무 저의 동이 그쪽이라서 정말 큰 소리가 났거든요. 정말 무슨 일이라도 난 것 같아서 창밖을 내다 봤는데 사람은 안 보이더라구요 혹시 무슨 일인지 아시..',
     date: '1시간전',
@@ -32,7 +32,7 @@ export const ANNONCEMENT_MOCK = [
   {
     avatar:
       'https://res.cloudinary.com/dh6tdcdyj/image/upload/v1695016765/KakaoTalk_20230918_145710613_id4fua.png',
-    title: '9월 23일 (수) 단수 예정입니다.',
+    title: '201동 화재 발생 신속히 건물밖으로 대피 바랍니다.',
     content:
       '어제 밤 한시쯤에 아파트 정문 부근에서 여자 비명소리 들으신 분 있나요? 너무 저의 동이 그쪽이라서 정말 큰 소리가 났거든요. 정말 무슨 일이라도 난 것 같아서 창밖을 내다 봤는데 사람은 안 보이더라구요 혹시 무슨 일인지 아시..',
     date: '1시간전',
@@ -41,96 +41,6 @@ export const ANNONCEMENT_MOCK = [
     writer: '나부터 살자',
     view: 63,
     like: 3,
-    comment: 3,
-    url: 'https://res.cloudinary.com/dh6tdcdyj/image/upload/v1695016765/KakaoTalk_20230918_145823493_hyqg3v.png',
-  },
-  {
-    avatar:
-      'https://res.cloudinary.com/dh6tdcdyj/image/upload/v1695016765/KakaoTalk_20230918_145710613_id4fua.png',
-    title: '9월 23일 (수) 단수 예정입니다.',
-    content:
-      '어제 밤 한시쯤에 아파트 정문 부근에서 여자 비명소리 들으신 분 있나요? 너무 저의 동이 그쪽이라서 정말 큰 소리가 났거든요. 정말 무슨 일이라도 난 것 같아서 창밖을 내다 봤는데 사람은 안 보이더라구요 혹시 무슨 일인지 아시..어제 밤 한시쯤에 아파트 정문 부근에서 여자 비명소리 들으신 분 있나요? 너무 저의 동이 그쪽이라서 정말 큰 소리가 났거든요. 정말 무슨 일이라도 난 것 같아서 창밖을 내다 봤는데 사람은 안 보이더라구요 혹시 무슨 일인지 아시..',
-    date: '1시간전',
-    urgency: '긴급',
-    job: '103동 경비원',
-    writer: '나부터 살자',
-    view: 63,
-    like: 4,
-    comment: 3,
-    url: '',
-  },
-  {
-    avatar:
-      'https://res.cloudinary.com/dh6tdcdyj/image/upload/v1695016765/KakaoTalk_20230918_145710613_id4fua.png',
-    title: '9월 23일 (수) 단수 예정입니다.',
-    content:
-      '어제 밤 한시쯤에 아파트 정문 부근에서 여자 비명소리 들으신 분 있나요? 너무 저의 동이 그쪽이라서 정말 큰 소리가 났거든요. 정말 무슨 일이라도 난 것 같아서 창밖을 내다 봤는데 사람은 안 보이더라구요 혹시 무슨 일인지 아시..',
-    date: '1시간전',
-    urgency: '일반',
-    job: '103동 경비원',
-    writer: '나부터 살자',
-    view: 63,
-    like: 5,
-    comment: 3,
-    url: '',
-  },
-  {
-    avatar:
-      'https://res.cloudinary.com/dh6tdcdyj/image/upload/v1695016765/KakaoTalk_20230918_145710613_id4fua.png',
-    title: '9월 23일 (수) 단수 예정입니다.',
-    content:
-      '어제 밤 한시쯤에 아파트 정문 부근에서 여자 비명소리 들으신 분 있나요? 너무 저의 동이 그쪽이라서 정말 큰 소리가 났거든요. 정말 무슨 일이라도 난 것 같아서 창밖을 내다 봤는데 사람은 안 보이더라구요 혹시 무슨 일인지 아시..',
-    date: '1시간전',
-    urgency: '긴급',
-    job: '103동 경비원',
-    writer: '나부터 살자',
-    view: 63,
-    like: 6,
-    comment: 3,
-    url: '',
-  },
-  {
-    avatar:
-      'https://res.cloudinary.com/dh6tdcdyj/image/upload/v1695016765/KakaoTalk_20230918_145710613_id4fua.png',
-    title: '9월 23일 (수) 단수 예정입니다.',
-    content:
-      '어제 밤 한시쯤에 아파트 정문 부근에서 여자 비명소리 들으신 분 있나요? 너무 저의 동이 그쪽이라서 정말 큰 소리가 났거든요. 정말 무슨 일이라도 난 것 같아서 창밖을 내다 봤는데 사람은 안 보이더라구요 혹시 무슨 일인지 아시..',
-    date: '1시간전',
-    urgency: '비상',
-    job: '103동 경비원',
-    writer: '나부터 살자',
-    view: 63,
-    like: 7,
-    comment: 3,
-    url: 'https://res.cloudinary.com/dh6tdcdyj/image/upload/v1695016765/KakaoTalk_20230918_145823493_hyqg3v.png',
-  },
-  {
-    avatar:
-      'https://res.cloudinary.com/dh6tdcdyj/image/upload/v1695016765/KakaoTalk_20230918_145710613_id4fua.png',
-    title: '9월 23일 (수) 단수 예정입니다.',
-    content:
-      '어제 밤 한시쯤에 아파트 정문 부근에서 여자 비명소리 들으신 분 있나요? 너무 저의 동이 그쪽이라서 정말 큰 소리가 났거든요. 정말 무슨 일이라도 난 것 같아서 창밖을 내다 봤는데 사람은 안 보이더라구요 혹시 무슨 일인지 아시..',
-    date: '1시간전',
-    urgency: '비상',
-    job: '103동 경비원',
-    writer: '나부터 살자',
-    view: 63,
-    like: 8,
-    comment: 3,
-    url: 'https://res.cloudinary.com/dh6tdcdyj/image/upload/v1695016765/KakaoTalk_20230918_145823493_hyqg3v.png',
-  },
-  {
-    avatar:
-      'https://res.cloudinary.com/dh6tdcdyj/image/upload/v1695016765/KakaoTalk_20230918_145710613_id4fua.png',
-    title: '9월 23일 (수) 단수 예정입니다.',
-    content:
-      '어제 밤 한시쯤에 아파트 정문 부근에서 여자 비명소리 들으신 분 있나요? 너무 저의 동이 그쪽이라서 정말 큰 소리가 났거든요. 정말 무슨 일이라도 난 것 같아서 창밖을 내다 봤는데 사람은 안 보이더라구요 혹시 무슨 일인지 아시..',
-    date: '1시간전',
-    urgency: '일반',
-    job: '103동 경비원',
-    writer: '나부터 살자',
-    view: 63,
-    like: 9,
     comment: 3,
     url: 'https://res.cloudinary.com/dh6tdcdyj/image/upload/v1695016765/KakaoTalk_20230918_145823493_hyqg3v.png',
   },
