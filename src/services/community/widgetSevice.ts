8080fd2f130c3a1c7ac9111c7914fb6499227ad6
HHB <bcl0206@gmail.com>
2023-10-22 11:17:40 +0900

    feat: 공지사항 위젯 실데이터 연결

--- src/services/community/widgetSevice.ts
+++ src/services/community/widgetSevice.ts
@@ -1,11 +1,31 @@
 import { AxiosResponse } from 'axios'
 import { instance } from 'configs/axios'
 
+interface ValidAnnounce {
+  aptId: string
+}
+
 interface SearchPostParam {
   keyword: string
 }
 
 export const widgetService = {
+  async getVaildAnnounce(param: ValidAnnounce) {
+    const { aptId } = param
+    try {
+      const response: AxiosResponse = await instance(`/api/${aptId}/announce/widget`, {
+        method: 'get',
+        data: {
+          floatFrom: '2023-10-21',
+          floatTo: '2023-10-30',
+        },
+      })
+      console.log('확인', response.data)
+    } catch (error) {
+      console.error(error)
+    }
+  },
+
   async getBestPosts() {
     try {
       const response: AxiosResponse = await instance('/api/article/best/view')
