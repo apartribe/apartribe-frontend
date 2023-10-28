6a0bd34f5e104ca6001ffed3520bd0ccf6ff0227
ChoongLim <bcl0206@naver.com>
2023-10-28 11:48:06 +0900

    feat: 캐러셸 라이브러리 제거 및 직접 구현

--- src/components/landing/AptRankSection.tsx
+++ src/components/landing/AptRankSection.tsx
@@ -1,6 +1,6 @@
 import { APT_RANK_MOCK } from 'mock/aptRankData'
 import React from 'react'
-import { styled } from 'styled-components'
+import { styled, keyframes } from 'styled-components'
 import { Container, Inner } from 'styles/reusable-style/layoutStyle'
 import AptRankCard from './AptRankCard'
 import Slider from 'react-slick'
@@ -8,30 +8,21 @@ import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 
 const AptRankSection = () => {
-  const settings = {
-    dots: false,
-    slidesToShow: 8,
-    slidesToScroll: 5, // 이걸로 속도올리면 버벅임 없음
-    infinite: true,
-    autoplay: true,
-    speed: 10000,
-    autoplaySpeed: 0,
-    cssEase: 'linear',
-    arrows: false,
-  }
-
   return (
     <Container>
+      <Inner>
+        <StyledParagraph>이 시간 가장 핫한 아파트 TOP 10</StyledParagraph>
+      </Inner>
       <StyledWrapper>
-        <Inner>
-          <StyledParagraph>지금 가장 핫한 아파트 TOP 10</StyledParagraph>
-        </Inner>
-        <StyledDiv>
-          <StyledSlider {...settings}>
-            {APT_RANK_MOCK.map((aptInfo, index) => (
-              <AptRankCard key={index} index={index} aptInfo={aptInfo} />
-            ))}
-          </StyledSlider>
+        <StyledDiv className="original">
+          {APT_RANK_MOCK.map((aptInfo, index) => (
+            <AptRankCard key={index} index={index} aptInfo={aptInfo} />
+          ))}
+        </StyledDiv>
+        <StyledDiv className="clone">
+          {APT_RANK_MOCK.map((aptInfo, index) => (
+            <AptRankCard key={index} index={index} aptInfo={aptInfo} />
+          ))}
         </StyledDiv>
       </StyledWrapper>
     </Container>
@@ -40,27 +31,69 @@ const AptRankSection = () => {
 
 export default AptRankSection
 
-const StyledWrapper = styled.div`
-  width: 100%;
-  height: 450px;
+const infiniteAnimation1 = keyframes`
+    0% {
+        
+        transform: translateX(0%);
+    }
+    50% {
+        visibility: hidden;
+        transform: translateX(-100%);
+    }
+    50.0001% {
+        transform: translateX(100%);
+        visibility: visible;
+    }
+    100% {
+        transform: translateX(0%);
+    }
 `
 
-const StyledDiv = styled.div`
-  width: 2000px;
+const infiniteAnimation2 = keyframes`
+    0% {
+        transform: translateX(0%);
+    }
+    100% {
+        transform: translateX(-200%);
+    }
+`
+
+const StyledDiv = styled.ul`
+  display: flex;
+  margin: 0;
+  gap: 20px;
+  // 상위 요소에 gap을 넣은 것은 한계점이 있음
+  // 하위 요소들의 사이에만 추가되는데, 좌우 위치가 뒤바뀌는 지금의 로직에서는
+  // 좌우가 전환 되었을 때, gap이 적용되지 않음.
+  // margin 사용시에도 겹쳐지는 한계 있음.
+  padding: 0 10px;
+
+  &.original {
+    animation: ${infiniteAnimation1} 50s linear infinite normal none running;
+  }
+
+  &.clone {
+    animation: ${infiniteAnimation2} 50s linear infinite;
+  }
+`
+
+const StyledWrapper = styled.div`
+  overflow: hidden;
   margin: 0 auto;
+  width: 2500px;
+  height: 350px;
+  display: flex; // 원리가 궁금하다면 주석 처리
+
+  &:hover > ${StyledDiv} {
+    animation-play-state: paused;
+  }
 `
 
 const StyledParagraph = styled.p`
   margin: 0;
-  padding: 20px;
+  padding-top: 30px;
   font-size: 30px;
   font-weight: 900;
   color: #303030;
-  /* line-height: 25px; */
-`
-
-const StyledSlider = styled(Slider)`
-  .slick-list {
-    overflow: visible;
-  }
+  line-height: 40px;
 `
