99b505ba9035bf9e048d31931cf5f76a079a3ee9
HHB <bcl0206@gmail.com>
2023-11-01 20:38:56 +0900

    feat: 토큰이 없을 시 reissue호출하지 않도록 로직 변경

--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,25 @@ import Router from 'routes/Router'
 import { Provider } from 'react-redux'
 import { store } from 'redux/store'
 import { auth } from 'services/auth'
+import {
+  getRefreshToken,
+  removeAccessToken,
+  removeRefreshToken,
+} from 'utils/localStorage'
 
 const root = createRoot(document.getElementById('root') as HTMLElement)
 
-const reIssue = () => {
-  auth.reissueToken()
+const reIssue = async () => {
+  if (!getRefreshToken()) return
+
+  const { statusCode } = await auth.reissueToken()
+
+  if (statusCode === 401) {
+    removeAccessToken()
+    removeRefreshToken()
+    return
+  }
+
   const timer = setInterval(() => {
     auth.reissueToken()
   }, 360000)
