c1cb000d3bb6643693e963f2a8d743bad46bbbf4
jieeeun2 <jieun2.dev@gmail.com>
2023-11-03 17:50:43 +0900

    feat: 로그인상태에 따라 헤더에 로그인 또는 설정페이지 표시되도록 수정

--- src/components/common/HeaderLanding.tsx
+++ src/components/common/HeaderLanding.tsx
@@ -3,10 +3,14 @@ import { LogoHeaderGradation, LogoHeaderGradationKorean } from 'assets/logos'
 import { Container, Inner } from 'styles/reusable-style/layoutStyle'
 import { NavLink } from 'react-router-dom'
 import { styled } from 'styled-components'
-import { IoPersonCircle } from 'react-icons/io5'
 import { LANDING_NAV_LIST } from 'constants/navList'
 import HeaderAptSearchBar from './apt-sugget-search-bar/HeaderAptSearchBar'
 import Slider from 'react-slick'
+import { PAGE_LOGIN } from 'constants/auth/path'
+import { PAGE_SETTING } from 'constants/setting/path'
+import { Img } from 'styles/reusable-style/elementStyle'
+import { useAppSelector } from 'hooks/useRedux'
+import defaultAvatar from 'assets/users/defaultAvatar.png'
 
 const HeaderLanding = () => {
   const [searchMode, setSearchMode] = useState<boolean>(false)
@@ -21,6 +25,8 @@ const HeaderLanding = () => {
     speed: 500,
     autoplaySpeed: 10000,
   }
+
+  const userInfo = useAppSelector((state) => state.user.userInfo)
 
   return (
     <Container $background="#FFFFFF">
@@ -57,9 +63,22 @@ const HeaderLanding = () => {
               {item.title}
             </StyledNavLink>
           ))}
-          <StyledNavLink to="/setting">
-            <IoPersonCircle fontSize="40px" color="#B3B3B3" cursor="pointer" />
-          </StyledNavLink>
+          {userInfo ? (
+            <StyledNavLink to={PAGE_SETTING}>
+              <Img
+                src={userInfo.profileImageUrl || defaultAvatar}
+                $width="40px"
+                $height="40px"
+                $borderRadius="50%"
+                $lineHeight="12px"
+                $margin="5px 0"
+              />
+            </StyledNavLink>
+          ) : (
+            <StyledNavLink to={PAGE_LOGIN} className="login">
+              로그인
+            </StyledNavLink>
+          )}
         </StyledDiv>
       </Inner>
     </Container>
@@ -101,6 +120,10 @@ const StyledNavLink = styled(NavLink)`
   &.active {
     font-weight: 900;
     color: #2b7f75;
+  }
+
+  &.login {
+    margin-bottom: -2px;
   }
 `
 
