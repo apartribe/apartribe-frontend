cc0c847a7ab0debd7b70732884cbe6cec75c2e0e
jieeeun2 <jieun2.dev@gmail.com>
2023-11-05 00:01:03 +0900

    feat: axios인터셉터 적용에 따른 api호출 메서드 형식 변경

--- src/services/apt/advertiseService.ts
+++ src/services/apt/advertiseService.ts
@@ -1,82 +1,79 @@
-import axios, { AxiosResponse } from 'axios'
+import axios from 'axios'
 import { instance } from 'configs/axios'
-import { Result, ContactInputValue } from 'types/setting'
+import { ResultWithMessage, ContactInputValue } from 'types/advertiseType'
 
 export const advertiseService = {
-  async sendEmail(name: string, email: string): Promise<Result> {
-    try {
-      await instance('/api/advertise/email/send', {
-        method: 'POST',
-        data: {
-          name: name,
-          email: email,
-        },
-      })
+  async sendEmail(name: string, email: string): Promise<ResultWithMessage> {
+    const response = await instance('/api/advertise/email/send', {
+      method: 'POST',
+      data: {
+        name: name,
+        email: email,
+      },
+    })
+
+    if (response.status === 200) {
       return {
         result: 'success',
         message: '이메일이 발송되었습니다. 메일함을 확인해주세요.',
       }
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        return {
-          result: 'fail',
-          message: '이메일 발송에 실패하였습니다. 다시 시도해주세요.',
-        }
-      } else {
-        throw new Error('different error than axios')
+    } else {
+      return {
+        result: 'fail',
+        message: '이메일 발송에 실패하였습니다. 다시 시도해주세요.',
       }
     }
   },
 
-  async confirmEmail(name: string, email: string, code: string): Promise<Result> {
-    console.log(name, email, code)
-    try {
-      /* const response: AxiosResponse =  */ await instance(
-        '/api/advertise/email/confirm',
-        {
-          method: 'GET',
-          params: {
-            name: name,
-            email: email,
-            code: code,
-          },
-        },
-      )
-      //if (response.data.isEmailTokenValid) {
-      return { result: 'success', message: '인증이 완료되었습니다.' }
-      /* } else {
-        return { result: 'fail', message: '인증번호가 일치하지 않습니다.' }
-      } */
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
+  async confirmEmail(
+    name: string,
+    email: string,
+    code: string,
+  ): Promise<ResultWithMessage> {
+    const response = await instance('/api/advertise/email/confirm', {
+      method: 'GET',
+      params: {
+        name: name,
+        email: email,
+        code: code,
+      },
+    })
+
+    if (response.status === 200) {
+      if (response.data.isEmailTokenValid) {
         return {
-          result: 'fail',
-          message: '인증번호 확인에 실패하였습니다. 다시 시도해주세요.',
+          result: 'success',
+          message: '인증이 완료되었습니다.',
         }
       } else {
-        throw new Error('different error than axios')
+        return {
+          result: 'fail',
+          message: '인증번호가 일치하지 않습니다.',
+        }
+      }
+    } else {
+      return {
+        result: 'fail',
+        message: '인증번호 확인에 실패하였습니다. 다시 시도해주세요.',
       }
     }
   },
 
-  async addAdvertise(body: ContactInputValue): Promise<Result> {
-    try {
-      await instance('/api/advertise/add', {
-        method: 'POST',
-        data: body,
-      })
+  async addAdvertise(body: ContactInputValue): Promise<ResultWithMessage> {
+    const response = await instance('/api/advertise/add', {
+      method: 'POST',
+      data: body,
+    })
+
+    if (response.status === 200) {
       return {
         result: 'success',
         message: '광고/제휴문의 등록이 완료되었습니다.',
       }
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        return {
-          result: 'fail',
-          message: '광고/제휴문의 등록에 실패하였습니다. 다시 시도해주세요.',
-        }
-      } else {
-        throw new Error('different error than axios')
+    } else {
+      return {
+        result: 'fail',
+        message: '광고/제휴문의 등록에 실패하였습니다. 다시 시도해주세요.',
       }
     }
   },
--- src/services/auth/userService.ts
+++ src/services/auth/userService.ts
@@ -1,107 +1,98 @@
-import axios from 'axios'
 import { instance } from 'configs/axios'
-import { ResultWithData, ResultWithMessage, ChangePwInputValue } from 'types/setting'
+import { ResultWithData, ResultWithMessage, ChangePwInputValue } from 'types/settingType'
 
 export const userService = {
   async showMember(email: string): Promise<ResultWithData | ResultWithMessage> {
-    try {
-      const response = await instance.get(`/api/member/${email}`)
+    const response = await instance.get(`/api/member/${email}`)
+
+    if (response.status === 200) {
       return {
         result: 'success',
         data: response.data.data,
       }
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        return {
-          result: 'fail',
-          message: '조회에 실패했습니다. 다시 시도해주세요.',
-        }
-      } else {
-        throw new Error('different error than axios')
+    } else {
+      return {
+        result: 'fail',
+        message: '조회에 실패했습니다. 다시 시도해주세요.',
       }
     }
   },
 
-  async updatePassword(body: ChangePwInputValue): Promise<ResultWithMessage> {
-    const { password, newPassword, newPasswordConfirm } = body
-    try {
-      await instance('/api/member/update/password', {
-        method: 'PUT',
-        data: {
-          currentPassword: password,
-          newPassword: newPassword,
-          passwordConfirm: newPasswordConfirm,
-        },
-      })
+  async updatePassword({
+    password,
+    newPassword,
+    newPasswordConfirm,
+  }: ChangePwInputValue): Promise<ResultWithMessage> {
+    const response = await instance('/api/member/update/password', {
+      method: 'PUT',
+      data: {
+        currentPassword: password,
+        newPassword: newPassword,
+        passwordConfirm: newPasswordConfirm,
+      },
+    })
+
+    if (response.status === 200) {
       return {
         result: 'success',
         message: '비밀번호가 변경되었습니다.',
       }
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        return {
-          result: 'fail',
-          message: '비밀번호 변경에 실패했습니다. 다시 시도해주세요.',
-        }
-      } else {
-        throw new Error('different error than axios')
+    } else if (response.status === 404) {
+      return {
+        result: 'fail',
+        message: '기존 비밀번호를 잘못 입력하였습니다. 다시 시도해주세요.',
+      }
+    } else {
+      return {
+        result: 'fail',
+        message: '비밀번호 변경에 실패했습니다. 다시 시도해주세요.',
       }
     }
   },
 
   async updateNickname(nickname: string): Promise<ResultWithMessage> {
-    try {
-      await instance('/api/member/update/nickname', {
-        method: 'PUT',
-        data: {
-          nickname: nickname,
-        },
-      })
+    const response = await instance('/api/member/update/nickname', {
+      method: 'PUT',
+      data: {
+        nickname: nickname,
+      },
+    })
+
+    if (response.status === 200) {
       return {
         result: 'success',
         message: '닉네임이 변경되었습니다.',
       }
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        if (error.response?.status === 400) {
-          return {
-            result: 'fail',
-            message:
-              error.response?.data.data.validation.nickname ||
-              error.response?.data.data.error,
-          }
-        } else {
-          return {
-            result: 'fail',
-            message: '닉네임 변경에 실패했습니다. 다시 시도해주세요.',
-          }
-        }
-      } else {
-        throw new Error('different error than axios')
+    } else if (response.status === 400) {
+      return {
+        result: 'fail',
+        message: response.data.data.validation.nickname || response.data.data.error,
+      }
+    } else {
+      return {
+        result: 'fail',
+        message: '닉네임 변경에 실패했습니다. 다시 시도해주세요.',
       }
     }
   },
 
   async updateImage(profileImageUrl: string): Promise<ResultWithMessage> {
-    try {
-      await instance('/api/member/update/image', {
-        method: 'PUT',
-        data: {
-          profileImageUrl: profileImageUrl,
-        },
-      })
+    const response = await instance('/api/member/update/image', {
+      method: 'PUT',
+      data: {
+        profileImageUrl: profileImageUrl,
+      },
+    })
+
+    if (response.status === 200) {
       return {
         result: 'success',
         message: '프로필 이미지가 변경되었습니다.',
       }
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        return {
-          result: 'fail',
-          message: '프로필 이미지 변경에 실패했습니다. 다시 시도해주세요.',
-        }
-      } else {
-        throw new Error('different error than axios')
+    } else {
+      return {
+        result: 'fail',
+        message: '프로필 이미지 변경에 실패했습니다. 다시 시도해주세요.',
       }
     }
   },
@@ -110,26 +101,23 @@ export const userService = {
     size: number,
     page: number,
   ): Promise<ResultWithData | ResultWithMessage> {
-    try {
-      const response = await instance('/api/member/article', {
-        method: 'GET',
-        params: {
-          size,
-          page,
-        },
-      })
+    const response = await instance('/api/member/article', {
+      method: 'GET',
+      params: {
+        size,
+        page,
+      },
+    })
+
+    if (response.status === 200) {
       return {
         result: 'success',
         data: response.data.data,
       }
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        return {
-          result: 'fail',
-          message: '조회에 실패했습니다. 다시 시도해주세요.',
-        }
-      } else {
-        throw new Error('different error than axios')
+    } else {
+      return {
+        result: 'fail',
+        message: '조회에 실패했습니다. 다시 시도해주세요.',
       }
     }
   },
@@ -138,26 +126,23 @@ export const userService = {
     size: number,
     page: number,
   ): Promise<ResultWithData | ResultWithMessage> {
-    try {
-      const response = await instance('/api/member/comment', {
-        method: 'GET',
-        params: {
-          size,
-          page,
-        },
-      })
+    const response = await instance('/api/member/comment', {
+      method: 'GET',
+      params: {
+        size,
+        page,
+      },
+    })
+
+    if (response.status === 200) {
       return {
         result: 'success',
         data: response.data.data,
       }
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        return {
-          result: 'fail',
-          message: '조회에 실패했습니다. 다시 시도해주세요.',
-        }
-      } else {
-        throw new Error('different error than axios')
+    } else {
+      return {
+        result: 'fail',
+        message: '조회에 실패했습니다. 다시 시도해주세요.',
       }
     }
   },
