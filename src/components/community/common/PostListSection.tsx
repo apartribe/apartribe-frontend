e0455fd6dac4e64c1a2820f9ff8e478a289e5fb4
ChoongLim <bcl0206@naver.com>
2023-11-19 23:37:47 +0900

    feat: 비로그인, 아파트 미인증자 대상 조건부 렌더링 구현 (상단 3개 게시물만 공개)

--- src/components/community/common/PostListSection.tsx
+++ src/components/community/common/PostListSection.tsx
@@ -8,8 +8,9 @@ import TogetherCard from '../together-page/togetherCard'
 import { ArticleCardType } from 'types/community-type/ArticleType'
 import { AnnounceCardType } from 'types/community-type/announceType'
 import { TogetherCardType } from 'types/community-type/togetherType'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { Sort } from 'types/community-type/sortType'
+import { useAppSelector } from 'hooks/useRedux'
 
 interface Props {
   boardType: BoardType
@@ -20,16 +21,17 @@ interface Props {
 const PostListSection: FC<Props> = ({ boardType, selectedCategory, selectedSort }) => {
   //=========================================================================
   const { aptId } = useParams()
+  const userInfo = useAppSelector((state) => state.user.userInfo)
+  const isNotVerifiedAptUser = userInfo.apartCode === '' || userInfo.apartCode === 'EMPTY'
 
   const [postList, setPostList] = useState<
     ArticleCardType[] | AnnounceCardType[] | TogetherCardType[]
   >([])
+  const [totalCount, setTotalCount] = useState(0)
   const [loading, setLoading] = useState(false)
   const [nothingToload, setNothingToload] = useState(false)
   const LoadingTargetRef = useRef(null)
   const pageCountRef = useRef(0)
-
-  // 무한 스크롤 ======추후 커스텀 훅으로 모듈화 고려
 
   useEffect(() => {
     // 카테고리 변경시 초기화 핵심 로직.
@@ -40,6 +42,27 @@ const PostListSection: FC<Props> = ({ boardType, selectedCategory, selectedSort 
   }, [selectedCategory, selectedSort, aptId])
 
   useEffect(() => {
+    // 비로그인, 아파트 미인증자 대상으로 실행됨
+    if (!isNotVerifiedAptUser) return
+    const getNewPage = async () => {
+      const response = await postsService.getPosts({
+        aptId: aptId as string,
+        boardType,
+        category: selectedCategory,
+        sort: selectedSort.value,
+        order: selectedSort.order,
+        page: 1,
+      })
+      setTotalCount(response.data.totalCount)
+      setPostList(response.data.results)
+    }
+
+    getNewPage()
+  }, [boardType, selectedSort, selectedCategory, aptId])
+
+  useEffect(() => {
+    // 아파트 인증자 대상으로 실행됨
+    if (isNotVerifiedAptUser) return
     const getNewPage = async () => {
       setLoading(true)
       pageCountRef.current = pageCountRef.current + 1
@@ -54,6 +77,7 @@ const PostListSection: FC<Props> = ({ boardType, selectedCategory, selectedSort 
       if (!response) return
       if (response.data.results.length === 0) return setNothingToload(true)
       setLoading(false)
+      setTotalCount(response.data.totalCount)
       setPostList((prev) => [...prev, ...response.data.results])
     }
 
@@ -80,13 +104,10 @@ const PostListSection: FC<Props> = ({ boardType, selectedCategory, selectedSort 
       observer.disconnect()
     }
   }, [loading, boardType, selectedSort, selectedCategory, aptId])
-  //======
-
-  // if (firstLoading) return <PostsLoading />
 
   return (
     <StyledDiv>
-      {postList.map((post) => {
+      {postList.slice(0, isNotVerifiedAptUser ? 3 : undefined).map((post) => {
         switch (boardType) {
           case 'article':
             return <ArticleCard key={post.id} post={post as ArticleCardType} />
@@ -96,11 +117,24 @@ const PostListSection: FC<Props> = ({ boardType, selectedCategory, selectedSort 
             return <TogetherCard key={post.id} post={post as TogetherCardType} />
         }
       })}
-      {nothingToload ? (
+      {isNotVerifiedAptUser && (
+        <>
+          {totalCount > 3 ? (
+            <StyledParagraph className="seeMore">
+              <StyledLink to="/setting/apartment-verification">
+                아파트 인증하고 {totalCount - 3}개 게시물 더보기
+              </StyledLink>
+            </StyledParagraph>
+          ) : (
+            <StyledParagraph>더이상 불러 올 게시물이 없습니다.</StyledParagraph>
+          )}
+        </>
+      )}
+      {!isNotVerifiedAptUser && nothingToload ? (
         <StyledParagraph>더이상 불러 올 게시물이 없습니다.</StyledParagraph>
       ) : (
         <StyledTarget ref={LoadingTargetRef}>
-          {loading && (
+          {!isNotVerifiedAptUser && loading && (
             <MoonLoader
               color="#36d7b7"
               size="40px"
@@ -125,13 +159,24 @@ const StyledDiv = styled.div`
 
 const StyledParagraph = styled.p`
   width: 870px;
-  height: 130px;
-  padding-top: 50px;
+  height: 50px;
+  padding-top: 15px;
   text-align: center;
+
+  &.seeMore {
+    &:hover {
+      transform: scale(1.05);
+    }
+  }
 `
 const StyledTarget = styled.div`
   width: 100%;
-  height: 160px;
+  height: 50px;
   display: flex;
   align-items: center;
+`
+
+const StyledLink = styled(Link)`
+  color: #2b7f75;
+  font-weight: 700;
 `
