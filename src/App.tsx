565170f7bca9e7733debb0c4c60e1097d5beb666
ChoongLim <bcl0206@naver.com>
2023-12-01 19:34:33 +0900

    feat: 토큰 저장 완료시 파라미터 제거 (보안성 고려)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Outlet, useSearchParams } from 'react-router-dom'
+import { Outlet, useNavigate, useSearchParams } from 'react-router-dom'
 import HeaderLanding from 'components/common/HeaderLanding'
 import Footer from 'components/common/Footer'
 import { ToastContainer } from 'react-toastify'
@@ -9,10 +9,13 @@ import { userService } from 'services/auth/userService'
 import { useDispatch } from 'react-redux'
 import { setLogin } from 'redux/store/userSlice'
 import { ResultWithData } from 'types/authType'
+import { useAppSelector } from 'hooks/useRedux'
 
 function App() {
+  const navigate = useNavigate()
   const dispatch = useDispatch()
-  const [searchParams] = useSearchParams()
+  const userInfo = useAppSelector((state) => state.user.userInfo)
+  const [searchParams, setSearchParams] = useSearchParams()
 
   const thirdPartyAccessToken = searchParams?.get('accessToken')
   const thirdPartyRefreshToken = searchParams?.get('refreshToken')
@@ -22,12 +25,14 @@ function App() {
 
   const showMember = async () => {
     if (!thirdPartyAccessToken || !thirdPartyRefreshToken) return
+    if (userInfo.email) return
     const showMemberResult = await userService.showMember()
     const { result, data } = showMemberResult as ResultWithData
 
     if (result === 'success') {
       dispatch(setLogin(data))
     }
+    setSearchParams({}) // 토큰이 param에 노출되어 있지 않도록, 사용 후 비워줌.
   }
 
   showMember()
